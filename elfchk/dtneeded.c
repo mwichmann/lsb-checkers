6f8e31e08904148936dc4ed6337a9aa146c83101
anderson <>
2001-02-07 04:08:58 -0400

    Sync in the data from version 0.4.3 so others can build/run the appchk tool

---
+++ elfchk/dtneeded.c
@@ -0,0 +1,19 @@
+char *DtNeeded[] = {
+	"ld-lsb.so.1",
+	"libGL.so.1.1",
+	"libICE.so.6",
+	"libSM.so.6",
+	"libX11.so.6",
+	"libXext.so.6",
+	"libXt.so.6",
+	"libc.so.6",
+	"libcrypt.so.1",
+	"libdb.so.3",
+	"libdl.so.2",
+	"libm.so.6",
+	"libncurses.so.4",
+	"libpthread.so.0",
+	"libutil.so.1",
+	"libz.so.1",
+	};
+int numDtNeeded = sizeof(DtNeeded)/sizeof(char *);
---
+++ elfchk/sectinfo.c
@@ -0,0 +1,42 @@
+#include "elfchk.h"
+#include "sections.h"
+
+struct SectionInfo SectionInfo[] = {
+	{".bss",SHT_NOBITS,SHF_ALLOC+SHF_WRITE,checkNOBITS},
+	{".comment",SHT_PROGBITS,0,checkPROGBITS},
+	{".ctors",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
+	{".data",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
+	{".data1",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
+	{".debug",SHT_PROGBITS,0,checkPROGBITS},
+	{".dtors",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
+	{".dynamic",SHT_DYNAMIC,SHF_ALLOC+SHF_WRITE,checkDYNAMIC},
+	{".dynstr",SHT_STRTAB,SHF_ALLOC,checkSTRTAB},
+	{".dynsym",SHT_DYNSYM,SHF_ALLOC,checkDYNSYM},
+	{".eh_frame",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
+	{".fini",SHT_PROGBITS,SHF_ALLOC+SHF_EXECINSTR,checkPROGBITS},
+	{".gnu.version",SHT_GNU_versym,SHF_ALLOC,checkGNU_versym},
+	{".gnu.version_r",SHT_GNU_verneed,SHF_ALLOC,checkGNU_verneed},
+	{".got",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
+	{".hash",SHT_HASH,SHF_ALLOC,checkHASH},
+	{".init",SHT_PROGBITS,SHF_ALLOC+SHF_EXECINSTR,checkPROGBITS},
+	{".interp",SHT_PROGBITS,SHF_ALLOC,checkPROGBITS},
+	{".line",SHT_PROGBITS,0,checkPROGBITS},
+	{".note",SHT_NOTE,0,checkNOTE},
+	{".note.ABI-tag",SHT_NOTE,SHF_ALLOC,checkNOTE},
+	{".plt",SHT_PROGBITS,SHF_ALLOC+SHF_EXECINSTR,checkPROGBITS},
+	{".rel.bss",SHT_REL,SHF_ALLOC,checkREL},
+	{".rel.data",SHT_REL,0,checkREL},
+	{".rel.got",SHT_REL,SHF_ALLOC,checkREL},
+	{".rel.plt",SHT_REL,SHF_ALLOC,checkREL},
+	{".rel.text",SHT_REL,0,checkREL},
+	{".rodata",SHT_PROGBITS,SHF_ALLOC,checkPROGBITS},
+	{".rodata1",SHT_PROGBITS,SHF_ALLOC,checkPROGBITS},
+	{".shstrtab",SHT_STRTAB,0,checkSTRTAB},
+	{".stab",SHT_PROGBITS,0,checkPROGBITS},
+	{".stabstr",SHT_STRTAB,0,checkSTRTAB},
+	{".strtab",SHT_STRTAB,SHF_ALLOC,checkSTRTAB},
+	{".symtab",SHT_SYMTAB,SHF_ALLOC,checkSYMTAB},
+	{".text",SHT_PROGBITS,SHF_ALLOC+SHF_EXECINSTR,checkPROGBITS},
+	};
+
+int numSectionInfo = sizeof(SectionInfo)/sizeof(struct SectionInfo);
