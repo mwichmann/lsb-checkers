f57616d81a0b57005a4dbedc492f72d7fd39a1c9
anderson <anderson>
2001-02-12 23:13:30 -0400

    New tool for testing the libs on a system to see if they contain the right stuff

---
+++ libchk/libs.c
@@ -0,0 +1,22 @@
+/* Generated file */
+#include "libs.h"
+
+check_libs()
+{
+check_lib("ld-lsb.so.1",ld_lsb_so_1);
+check_lib("libGL.so.1.1",libGL_so_1_1);
+check_lib("libICE.so.6",libICE_so_6);
+check_lib("libSM.so.6",libSM_so_6);
+check_lib("libX11.so.6",libX11_so_6);
+check_lib("libXext.so.6",libXext_so_6);
+check_lib("libXt.so.6",libXt_so_6);
+check_lib("libc.so.6",libc_so_6);
+check_lib("libcrypt.so.1",libcrypt_so_1);
+check_lib("libdb.so.3",libdb_so_3);
+check_lib("libdl.so.2",libdl_so_2);
+check_lib("libm.so.6",libm_so_6);
+check_lib("libncurses.so.4",libncurses_so_4);
+check_lib("libpthread.so.0",libpthread_so_0);
+check_lib("libutil.so.1",libutil_so_1);
+check_lib("libz.so.1",libz_so_1);
+}
---
+++ libchk/libs.mk
@@ -0,0 +1,18 @@
+# Generated file #
+LIBOBJS = \
+	ld_lsb_so_1.o \
+	libGL_so_1_1.o \
+	libICE_so_6.o \
+	libSM_so_6.o \
+	libX11_so_6.o \
+	libXext_so_6.o \
+	libXt_so_6.o \
+	libc_so_6.o \
+	libcrypt_so_1.o \
+	libdb_so_3.o \
+	libdl_so_2.o \
+	libm_so_6.o \
+	libncurses_so_4.o \
+	libpthread_so_0.o \
+	libutil_so_1.o \
+	libz_so_1.o \
