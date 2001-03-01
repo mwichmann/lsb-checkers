2984247bfa420bea5e408fda015f88d73d89ba81
anderson <anderson>
2001-03-01 19:19:40 -0400

    Fix the lib versions on GL & ncurses Updated interfaces lists for 0.5.2+

--- devchk/unistd_h.c
+++ devchk/unistd_h.c
@@ -1,16 +1,3 @@
 /*
- * Test of unistd.h
+ * No values in unistd.h
  */
-#include "hdrchk.h"
-#include "sys/types.h"
-#include "unistd.h"
-
-
-
-int unistd_h()
-{
-int cnt=0;
-CheckTypeSize(ssize_t,4)
-printf("%d tests in unistd.h\n",cnt);
-return cnt;
-}
--- libchk/libs.c
+++ libchk/libs.c
@@ -4,7 +4,7 @@
 check_libs()
 {
 check_lib("ld-lsb.so.1",ld_lsb_so_1);
-check_lib("libGL.so.1.1",libGL_so_1_1);
+check_lib("libGL.so.1.2",libGL_so_1_2);
 check_lib("libICE.so.6",libICE_so_6);
 check_lib("libSM.so.6",libSM_so_6);
 check_lib("libX11.so.6",libX11_so_6);
@@ -15,8 +15,9 @@ check_lib("libcrypt.so.1",libcrypt_so_1);
 check_lib("libdb.so.3",libdb_so_3);
 check_lib("libdl.so.2",libdl_so_2);
 check_lib("libm.so.6",libm_so_6);
-check_lib("libncurses.so.4",libncurses_so_4);
+check_lib("libncurses.so.5",libncurses_so_5);
 check_lib("libpthread.so.0",libpthread_so_0);
+check_lib("librt.so.1",librt_so_1);
 check_lib("libutil.so.1",libutil_so_1);
 check_lib("libz.so.1",libz_so_1);
 }
