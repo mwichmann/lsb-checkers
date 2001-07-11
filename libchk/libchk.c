e5f3d12ec82c7de50991afc7023f7009e007e6a6
cyeoh <cyeoh>
2001-07-11 04:23:02 -0400

    Rework of file. Fixes two bugs - not checking versions of 'hidden' symbols - only search the first occurrence of the symbol instead of all.

--- libchk/libchk.c
+++ libchk/libchk.c
@@ -1,11 +1,17 @@
-#include <unistd.h>
-#include "elfchk.h"
+/*
+ * libchk.c 
+ *
+ * Copyright (c) 2001 The Free Standards Group Inc.
+ *
+ * Stuart Anderson (anderson@metrolink.com)
+ * Chris Yeoh (yeohc@au.ibm.com)
+ *
+ */
 
-main(int argc, char *argv[])
-{
-printf("%s " LSBVERSION "\n", argv[0]);
-check_libs();
-}
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include "elfchk.h"
 
 char *libpaths[] = {
 	"/lib/%s",
@@ -13,92 +19,162 @@ char *libpaths[] = {
 	"/usr/X11R6/lib/%s",
 	0 };
 
-check_lib(char *libname,struct versym entries[])
+/* Returns 1 on match, 0 otherwise */
+int
+check_symbol(ElfFile *file, struct versym *entry, int print_warnings)
 {
-ElfFile		*file=NULL;
-Elf32_Shdr	*hdr;
-char filename[128];
-int i,j;
+  int j;
+  /* See if this symbol is in the dynsyn section of the library */
 
-/* Find the library */
-
-for(i=0;libpaths[i];i++) {
-	sprintf(filename,libpaths[i],libname);
-	if( access(filename,R_OK) == 0 ) {
-		file=OpenElfFile(filename);
-		if(file) break;
-		}
-	}
-
-if( !file ) {
-	printf("Unable to find library %s\n", libname );
-	return;
-	}
-
-checkElfhdr(file);
-
-printf("Checking symbols in %s\n", filename );
-
-for(i=0;entries[i].name;i++) {
-	/* See if this symbol is in the dynsyn section of the library */
-
-	/* printf("Looking for %s\n", entries[i].name); */
-	for(j=0;j<file->numsyms;j++) {
-		if( !( ELF32_ST_TYPE(file->syms[j].st_info) == STT_FUNC ||
-		       ELF32_ST_TYPE(file->syms[j].st_info) == STT_OBJECT) )
-			continue;
+  /* printf("Looking for %s\n", entry->name); */
+  for (j=0; j<file->numsyms; j++) 
+  {
+    if (! (ELF32_ST_TYPE(file->syms[j].st_info) == STT_FUNC ||
+           ELF32_ST_TYPE(file->syms[j].st_info) == STT_OBJECT))
+      continue;
 #ifdef DEBUG
-		printf("Bind=%x\n", ELF32_ST_BIND(file->syms[j].st_info) );
-		printf("Type=%x\n", ELF32_ST_TYPE(file->syms[j].st_info) );
-		printf("Comparing %s and %s\n", 
-			ElfGetStringIndex(file,file->syms[j].st_name,
-				file->symhdr->sh_link),entries[i].name);
+    printf("Bind=%x\n", ELF32_ST_BIND(file->syms[j].st_info) );
+    printf("Type=%x\n", ELF32_ST_TYPE(file->syms[j].st_info) );
+    printf("Comparing %s and %s\n", 
+           ElfGetStringIndex(file,file->syms[j].st_name,
+                             file->symhdr->sh_link),entry->name);
 #endif
-		if( strcmp(ElfGetStringIndex(file,file->syms[j].st_name,
-				file->symhdr->sh_link),entries[i].name) == 0 )
-			break;
-		}
-	if( j == file->numsyms ) {
-		printf("Didn't find %s in %s\n", entries[i].name,libname);
-		continue;
-		}
+    if (strcmp(ElfGetStringIndex(file,file->syms[j].st_name,
+                                 file->symhdr->sh_link),entry->name) 
+        == 0)
+    {
 
-	/* Now, check to see if it has the right version associated with it */
+      /* Now, check to see if it has the right version associated with it */
 
-	/* This bit means it's internal */
-	if( file->vers[j] & 0x8000 )
-		continue;
+      /* This bit means it's internal */
+      Elf32_Half vers = file->vers[j];
+      if (vers & 0x8000)
+      {
+        vers = vers & 0x7FFF;
+      }
 
-	/* Zero means the symbol is local */
-	if( file->vers[j] == 0 )
-		continue;
+      /* Zero means the symbol is local */
+      if (vers == 0)
+        continue;
 
-	/* One means the symbol is defined & available, but not versioned */
-	if( file->vers[j] == 1 ) {
-		if( entries[i].vername[0] != '\000' )
-		printf("Warning!!! Found unversioned symbol %s, but expecting version %s\n",
-		ElfGetStringIndex(file,file->syms[j].st_name,file->symhdr->sh_link),
-		entries[i].vername);
-		continue;
-		}
+      /* One means the symbol is defined & available, but not versioned */
+      if (vers == 1) 
+      {
+        if (entry->vername[0] != '\000')
+        {
+          if (print_warnings)
+          {
+            printf("    Warning!!! Found unversioned symbol %s,"
+                   "but expecting version %s\n",
+                   ElfGetStringIndex(file,file->syms[j].st_name,
+                                     file->symhdr->sh_link),
+                   entry->vername);
+          }
+          continue;
+        }
+        else
+        {
+          /* Found and expected unversioned symbol */
+          return 1;
+        }
+      }
 
-	/* This is just a sanity check, which should never be hit */
-	if( file->vers[j] > file->numverdefs ) {
-		printf("Warning!!! Found version %x, for %s which is too big\n",
-		file->vers[j],
-		ElfGetStringIndex(file,file->syms[j].st_name,file->symhdr->sh_link));
-		continue;
-	}
+      /* This is just a sanity check, which should never be hit */
+      if (vers > file->numverdefs) 
+      {
+        if (print_warnings)
+        {
+          printf("    Warning!!! Found version %x, for %s which is too big\n",
+                 vers,
+                 ElfGetStringIndex(file,file->syms[j].st_name,
+                                   file->symhdr->sh_link));
+        }
+        continue;
+      }
+
+      if (strcmp(file->versionnames[vers], entry->vername) != 0) 
+      {
+        if (print_warnings)
+        {
+          printf("    %s has version %s, expecting ",
+                 ElfGetStringIndex(file,file->syms[j].st_name,
+                                   file->symhdr->sh_link),
+                 file->versionnames[vers]);
+          if (strlen(entry->vername)>0)
+            printf("%s\n", entry->vername);
+          else
+            printf("unversioned\n");
 
 #ifdef DEBUG
-	printf("Symbol %s vers %s\n",
-		ElfGetStringIndex(file,file->syms[j].st_name,file->symhdr->sh_link),
-		file->versionnames[file->vers[j]]);
+      printf("Symbol %s vers %s\n",
+             ElfGetStringIndex(file,file->syms[j].st_name,
+                               file->symhdr->sh_link),
+             file->versionnames[vers]);
 #endif
-	if( strcmp(file->versionnames[file->vers[j]], entries[i].vername) != 0 ) {
-		printf("%s has version %s, expecting %s\n",
-			ElfGetStringIndex(file,file->syms[j].st_name,file->symhdr->sh_link),
-			file->versionnames[file->vers[j]], entries[i].vername );
-		}
-	}
+
+        }
+      }
+      else
+      {
+        /* Found match */
+        return 1;
+      }
+    }
+  }
+
+  /* Did not find exact match for symbol */
+  return 0;
 }
+
+
+void
+check_lib(char *libname, struct versym entries[])
+{
+  ElfFile *file = NULL;
+  char filename[PATH_MAX+1];
+  int i;
+
+  /* Find the library */
+  for(i=0; libpaths[i]; i++)
+  {
+    snprintf(filename, PATH_MAX, libpaths[i], libname);
+    if (access(filename,R_OK) == 0) 
+    {
+      file=OpenElfFile(filename);
+      if(file) break;
+    }
+  }
+
+  if(file==NULL) 
+  {
+    printf("Unable to find library %s\n", libname );
+    return;
+  }
+
+  checkElfhdr(file);
+
+  printf("Checking symbols in %s\n", filename );
+
+  for (i=0; entries[i].name; i++) 
+  {
+    if(!check_symbol(file, entries+i, 0))
+    {
+      /* Failed to match */
+      printf("  Didn't find %s ", entries[i].name);
+      if (strlen(entries[i].vername)>0) printf("(%s)", entries[i].vername);
+      else printf("(unversioned)");
+      printf(" in %s\n", libname);
+      check_symbol(file, entries+i, 1);
+    }
+  }
+}
+
+
+
+int main(int argc, char *argv[])
+{
+  printf("%s " LSBVERSION "\n", argv[0]);
+  check_libs();
+  exit(0);
+}
+
