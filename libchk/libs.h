c7dea73b52a1559047e32e15d33b348f0702c740
cyeoh <>
2002-03-20 05:37:38 -0400

    Adds tet journal reporting General cleanup

--- libchk/libs.h
+++ libchk/libs.h
@@ -1,35 +1,37 @@
 /* Generated file */
+#include "../elfchk/elfchk.h"
+
 #if defined(__ia64__)
-extern char *ld_lsb_ia64_so_1[];
+extern struct versym ld_lsb_ia64_so_1[];
 #endif
 #if defined(__i386__)
-extern char *ld_lsb_so_1[];
+extern struct versym ld_lsb_so_1[];
 #endif
-extern char *libGL_so_1[];
-extern char *libICE_so_6[];
-extern char *libSM_so_6[];
-extern char *libX11_so_6[];
-extern char *libXext_so_6[];
-extern char *libXt_so_6[];
+extern struct versym libGL_so_1[];
+extern struct versym libICE_so_6[];
+extern struct versym libSM_so_6[];
+extern struct versym libX11_so_6[];
+extern struct versym libXext_so_6[];
+extern struct versym libXt_so_6[];
 #if defined(__i386__)
-extern char *libc_so_6[];
+extern struct versym libc_so_6[];
 #endif
 #if defined(__ia64__)
-extern char *libc_so_6_1[];
+extern struct versym libc_so_6_1[];
 #endif
-extern char *libcrypt_so_1[];
-extern char *libdl_so_2[];
+extern struct versym libcrypt_so_1[];
+extern struct versym libdl_so_2[];
 #if defined(__i386__)
-extern char *libm_so_6[];
+extern struct versym libm_so_6[];
 #endif
-extern char *libncurses_so_5[];
+extern struct versym libncurses_so_5[];
 #if defined(__i386__)
-extern char *libpthread_so_0[];
-#endif
-#if defined(__i386__)
-extern char *librt_so_1[];
+extern struct versym libpthread_so_0[];
 #endif
 #if defined(__i386__)
-extern char *libutil_so_1[];
+extern struct versym librt_so_1[];
 #endif
-extern char *libz_so_1[];
+#if defined(__i386__)
+extern struct versym libutil_so_1[];
+#endif
+extern struct versym libz_so_1[];
