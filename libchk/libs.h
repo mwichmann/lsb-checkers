dedcaeb33f6551361806bdd94b7135c33676b0a9
anderson <>
2002-03-25 20:37:06 -0400

    Split the Library table into Library and ArchLib, analagous to how other tables have been done. This allows the library runname to be architecture sensitvie. Scripts and the resulting output C code should now correctly reflect the right library names.

--- libchk/libs.h
+++ libchk/libs.h
@@ -7,12 +7,6 @@ extern struct versym ld_lsb_ia64_so_1[];
 #if defined(__i386__)
 extern struct versym ld_lsb_so_1[];
 #endif
-extern struct versym libGL_so_1[];
-extern struct versym libICE_so_6[];
-extern struct versym libSM_so_6[];
-extern struct versym libX11_so_6[];
-extern struct versym libXext_so_6[];
-extern struct versym libXt_so_6[];
 #if defined(__i386__)
 extern struct versym libc_so_6[];
 #endif
@@ -21,6 +15,8 @@ extern struct versym libc_so_6_1[];
 #endif
 extern struct versym libcrypt_so_1[];
 extern struct versym libdl_so_2[];
+extern struct versym libGL_so_1[];
+extern struct versym libICE_so_6[];
 #if defined(__i386__)
 extern struct versym libm_so_6[];
 #endif
@@ -28,9 +24,11 @@ extern struct versym libm_so_6[];
 extern struct versym libm_so_6_1[];
 #endif
 extern struct versym libncurses_so_5[];
-#if defined(__i386__)
 extern struct versym libpthread_so_0[];
-#endif
 extern struct versym librt_so_1[];
+extern struct versym libSM_so_6[];
 extern struct versym libutil_so_1[];
+extern struct versym libX11_so_6[];
+extern struct versym libXext_so_6[];
+extern struct versym libXt_so_6[];
 extern struct versym libz_so_1[];
--- libchk/libs.mk
+++ libchk/libs.mk
@@ -2,20 +2,20 @@
 LIBOBJS = \
 	ld_lsb_ia64_so_1.o \
 	ld_lsb_so_1.o \
-	libGL_so_1.o \
-	libICE_so_6.o \
-	libSM_so_6.o \
-	libX11_so_6.o \
-	libXext_so_6.o \
-	libXt_so_6.o \
 	libc_so_6.o \
 	libc_so_6_1.o \
 	libcrypt_so_1.o \
 	libdl_so_2.o \
+	libGL_so_1.o \
+	libICE_so_6.o \
 	libm_so_6.o \
 	libm_so_6_1.o \
 	libncurses_so_5.o \
 	libpthread_so_0.o \
 	librt_so_1.o \
+	libSM_so_6.o \
 	libutil_so_1.o \
+	libX11_so_6.o \
+	libXext_so_6.o \
+	libXt_so_6.o \
 	libz_so_1.o \
