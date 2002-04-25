cd56306cee10cd2d1a57fcdd76e86fed6f71b4ad
anderson <anderson>
2002-04-25 14:06:00 -0400

    IA64 specific data now included

--- libchk/libs.h
+++ libchk/libs.h
@@ -13,8 +13,20 @@ extern struct versym libc_so_6[];
 #if defined(__ia64__)
 extern struct versym libc_so_6_1[];
 #endif
+#if defined(__ia64__)
+extern struct versym libcrypt_so_1[];
+#endif
+#if defined(__i386__)
+extern struct versym libcrypt_so_1[];
+#endif
 extern struct versym libcrypt_so_1[];
 extern struct versym libdl_so_2[];
+#if defined(__ia64__)
+extern struct versym libdl_so_2[];
+#endif
+#if defined(__i386__)
+extern struct versym libdl_so_2[];
+#endif
 extern struct versym libGL_so_1[];
 extern struct versym libICE_so_6[];
 #if defined(__i386__)
@@ -24,7 +36,13 @@ extern struct versym libm_so_6[];
 extern struct versym libm_so_6_1[];
 #endif
 extern struct versym libncurses_so_5[];
+#if defined(__i386__)
 extern struct versym libpthread_so_0[];
+#endif
+extern struct versym libpthread_so_0[];
+#if defined(__ia64__)
+extern struct versym libpthread_so_0[];
+#endif
 extern struct versym libSM_so_6[];
 extern struct versym libutil_so_1[];
 extern struct versym libX11_so_6[];
