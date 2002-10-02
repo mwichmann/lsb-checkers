e4201bb4e3bb052187527af29f7a58a9a43be462
anderson <>
2002-10-02 14:13:07 -0400

    Sync in recent additions

---
+++ libchk/libpam_so_0.c
@@ -0,0 +1,16 @@
+#include "elfchk.h"
+struct versym libpam_so_0[] = {
+	{"pam_acct_mgmt",""},
+	{"pam_authenticate",""},
+	{"pam_chauthtok",""},
+	{"pam_close_session",""},
+	{"pam_end",""},
+	{"pam_fail_delay",""},
+	{"pam_get_item",""},
+	{"pam_getenvlist",""},
+	{"pam_open_session",""},
+	{"pam_set_item",""},
+	{"pam_setcred",""},
+	{"pam_start",""},
+	{"pam_strerror",""},
+	{0,0}};
--- libchk/libs.mk
+++ libchk/libs.mk
@@ -1,5 +1,6 @@
 # Generated file #
 LIBOBJS = \
+	ld_lsb_64_so_1.o \
 	ld_lsb_ia64_so_1.o \
 	ld_lsb_so_1.o \
 	libc_so_6.o \
@@ -11,8 +12,10 @@ LIBOBJS = \
 	libm_so_6.o \
 	libm_so_6_1.o \
 	libncurses_so_5.o \
+	libpam_so_0.o \
 	libpthread_so_0.o \
 	libSM_so_6.o \
+	libstdcxx_so_5.o \
 	libutil_so_1.o \
 	libX11_so_6.o \
 	libXext_so_6.o \
