d0f3419004a1d9cabc639dcb66115a1edc34a292
anderson <>
2002-10-08 20:18:45 -0400

    drop duplicate entries caused by messed up symbol version info in the DB

--- libchk/libncurses_so_5.c
+++ libchk/libncurses_so_5.c
@@ -1,1100 +1,278 @@
 #include "elfchk.h"
 struct versym libncurses_so_5[] = {
 	{"COLS",""},
-#if defined(__i386__)
-	{"COLS",""},
-#endif
 	{"LINES",""},
-#if defined(__i386__)
-	{"LINES",""},
-#endif
 	{"addch",""},
-#if defined(__i386__)
-	{"addch",""},
-#endif
 	{"addchnstr",""},
-#if defined(__i386__)
-	{"addchnstr",""},
-#endif
 	{"addchstr",""},
-#if defined(__i386__)
-	{"addchstr",""},
-#endif
 	{"addnstr",""},
-#if defined(__i386__)
-	{"addnstr",""},
-#endif
 	{"addstr",""},
-#if defined(__i386__)
-	{"addstr",""},
-#endif
 	{"attr_get",""},
-#if defined(__i386__)
-	{"attr_get",""},
-#endif
 	{"attr_off",""},
-#if defined(__i386__)
-	{"attr_off",""},
-#endif
 	{"attr_on",""},
-#if defined(__i386__)
-	{"attr_on",""},
-#endif
 	{"attr_set",""},
-#if defined(__i386__)
-	{"attr_set",""},
-#endif
 	{"attroff",""},
-#if defined(__i386__)
-	{"attroff",""},
-#endif
 	{"attron",""},
-#if defined(__i386__)
-	{"attron",""},
-#endif
 	{"attrset",""},
-#if defined(__i386__)
-	{"attrset",""},
-#endif
 	{"baudrate",""},
-#if defined(__i386__)
-	{"baudrate",""},
-#endif
 	{"beep",""},
-#if defined(__i386__)
-	{"beep",""},
-#endif
 	{"bkgd",""},
-#if defined(__i386__)
-	{"bkgd",""},
-#endif
 	{"bkgdset",""},
-#if defined(__i386__)
-	{"bkgdset",""},
-#endif
 	{"border",""},
-#if defined(__i386__)
-	{"border",""},
-#endif
 	{"box",""},
-#if defined(__i386__)
-	{"box",""},
-#endif
 	{"can_change_color",""},
-#if defined(__i386__)
-	{"can_change_color",""},
-#endif
 	{"cbreak",""},
-#if defined(__i386__)
-	{"cbreak",""},
-#endif
 	{"chgat",""},
-#if defined(__i386__)
-	{"chgat",""},
-#endif
 	{"clear",""},
-#if defined(__i386__)
-	{"clear",""},
-#endif
 	{"clearok",""},
-#if defined(__i386__)
-	{"clearok",""},
-#endif
 	{"clrtobot",""},
-#if defined(__i386__)
-	{"clrtobot",""},
-#endif
 	{"clrtoeol",""},
-#if defined(__i386__)
-	{"clrtoeol",""},
-#endif
 	{"color_content",""},
-#if defined(__i386__)
-	{"color_content",""},
-#endif
 	{"color_set",""},
-#if defined(__i386__)
-	{"color_set",""},
-#endif
 	{"copywin",""},
-#if defined(__i386__)
-	{"copywin",""},
-#endif
 	{"curs_set",""},
-#if defined(__i386__)
-	{"curs_set",""},
-#endif
 	{"curscr",""},
-#if defined(__i386__)
-	{"curscr",""},
-#endif
 	{"def_prog_mode",""},
-#if defined(__i386__)
-	{"def_prog_mode",""},
-#endif
 	{"def_shell_mode",""},
-#if defined(__i386__)
-	{"def_shell_mode",""},
-#endif
 	{"delay_output",""},
-#if defined(__i386__)
-	{"delay_output",""},
-#endif
 	{"delch",""},
-#if defined(__i386__)
-	{"delch",""},
-#endif
 	{"deleteln",""},
-#if defined(__i386__)
-	{"deleteln",""},
-#endif
 	{"delscreen",""},
-#if defined(__i386__)
-	{"delscreen",""},
-#endif
 	{"delwin",""},
-#if defined(__i386__)
-	{"delwin",""},
-#endif
 	{"derwin",""},
-#if defined(__i386__)
-	{"derwin",""},
-#endif
 	{"doupdate",""},
-#if defined(__i386__)
-	{"doupdate",""},
-#endif
 	{"dupwin",""},
-#if defined(__i386__)
-	{"dupwin",""},
-#endif
 	{"echo",""},
-#if defined(__i386__)
-	{"echo",""},
-#endif
 	{"echochar",""},
-#if defined(__i386__)
-	{"echochar",""},
-#endif
 	{"endwin",""},
-#if defined(__i386__)
-	{"endwin",""},
-#endif
 	{"erase",""},
-#if defined(__i386__)
-	{"erase",""},
-#endif
 	{"erasechar",""},
-#if defined(__i386__)
-	{"erasechar",""},
-#endif
 	{"filter",""},
-#if defined(__i386__)
-	{"filter",""},
-#endif
 	{"flash",""},
-#if defined(__i386__)
-	{"flash",""},
-#endif
 	{"flushinp",""},
-#if defined(__i386__)
-	{"flushinp",""},
-#endif
 	{"getbkgd",""},
-#if defined(__i386__)
-	{"getbkgd",""},
-#endif
 	{"getch",""},
-#if defined(__i386__)
-	{"getch",""},
-#endif
 	{"getnstr",""},
-#if defined(__i386__)
-	{"getnstr",""},
-#endif
 	{"getstr",""},
-#if defined(__i386__)
-	{"getstr",""},
-#endif
 	{"getwin",""},
-#if defined(__i386__)
-	{"getwin",""},
-#endif
 	{"halfdelay",""},
-#if defined(__i386__)
-	{"halfdelay",""},
-#endif
 	{"has_colors",""},
-#if defined(__i386__)
-	{"has_colors",""},
-#endif
 	{"has_ic",""},
-#if defined(__i386__)
-	{"has_ic",""},
-#endif
 	{"has_il",""},
-#if defined(__i386__)
-	{"has_il",""},
-#endif
 	{"hline",""},
-#if defined(__i386__)
-	{"hline",""},
-#endif
 	{"idcok",""},
-#if defined(__i386__)
-	{"idcok",""},
-#endif
 	{"idlok",""},
-#if defined(__i386__)
-	{"idlok",""},
-#endif
 	{"immedok",""},
-#if defined(__i386__)
-	{"immedok",""},
-#endif
 	{"inch",""},
-#if defined(__i386__)
-	{"inch",""},
-#endif
 	{"inchnstr",""},
-#if defined(__i386__)
-	{"inchnstr",""},
-#endif
 	{"inchstr",""},
-#if defined(__i386__)
-	{"inchstr",""},
-#endif
 	{"init_color",""},
-#if defined(__i386__)
-	{"init_color",""},
-#endif
 	{"init_pair",""},
-#if defined(__i386__)
-	{"init_pair",""},
-#endif
 	{"initscr",""},
-#if defined(__i386__)
-	{"initscr",""},
-#endif
 	{"innstr",""},
-#if defined(__i386__)
-	{"innstr",""},
-#endif
 	{"insch",""},
-#if defined(__i386__)
-	{"insch",""},
-#endif
 	{"insdelln",""},
-#if defined(__i386__)
-	{"insdelln",""},
-#endif
 	{"insertln",""},
-#if defined(__i386__)
-	{"insertln",""},
-#endif
 	{"insnstr",""},
-#if defined(__i386__)
-	{"insnstr",""},
-#endif
 	{"insstr",""},
-#if defined(__i386__)
-	{"insstr",""},
-#endif
 	{"instr",""},
-#if defined(__i386__)
-	{"instr",""},
-#endif
 	{"intrflush",""},
-#if defined(__i386__)
-	{"intrflush",""},
-#endif
 	{"is_linetouched",""},
-#if defined(__i386__)
-	{"is_linetouched",""},
-#endif
 	{"is_wintouched",""},
-#if defined(__i386__)
-	{"is_wintouched",""},
-#endif
 	{"isendwin",""},
-#if defined(__i386__)
-	{"isendwin",""},
-#endif
 	{"keyname",""},
-#if defined(__i386__)
-	{"keyname",""},
-#endif
 	{"keypad",""},
-#if defined(__i386__)
-	{"keypad",""},
-#endif
 	{"killchar",""},
-#if defined(__i386__)
-	{"killchar",""},
-#endif
 	{"leaveok",""},
-#if defined(__i386__)
-	{"leaveok",""},
-#endif
 	{"longname",""},
-#if defined(__i386__)
-	{"longname",""},
-#endif
 	{"meta",""},
-#if defined(__i386__)
-	{"meta",""},
-#endif
 	{"move",""},
-#if defined(__i386__)
-	{"move",""},
-#endif
 	{"mvaddch",""},
-#if defined(__i386__)
-	{"mvaddch",""},
-#endif
 	{"mvaddchnstr",""},
-#if defined(__i386__)
-	{"mvaddchnstr",""},
-#endif
 	{"mvaddchstr",""},
-#if defined(__i386__)
-	{"mvaddchstr",""},
-#endif
 	{"mvaddnstr",""},
-#if defined(__i386__)
-	{"mvaddnstr",""},
-#endif
 	{"mvaddstr",""},
-#if defined(__i386__)
-	{"mvaddstr",""},
-#endif
 	{"mvchgat",""},
-#if defined(__i386__)
-	{"mvchgat",""},
-#endif
 	{"mvcur",""},
-#if defined(__i386__)
-	{"mvcur",""},
-#endif
 	{"mvdelch",""},
-#if defined(__i386__)
-	{"mvdelch",""},
-#endif
 	{"mvderwin",""},
-#if defined(__i386__)
-	{"mvderwin",""},
-#endif
 	{"mvgetch",""},
-#if defined(__i386__)
-	{"mvgetch",""},
-#endif
 	{"mvgetnstr",""},
-#if defined(__i386__)
-	{"mvgetnstr",""},
-#endif
 	{"mvgetstr",""},
-#if defined(__i386__)
-	{"mvgetstr",""},
-#endif
 	{"mvhline",""},
-#if defined(__i386__)
-	{"mvhline",""},
-#endif
 	{"mvinch",""},
-#if defined(__i386__)
-	{"mvinch",""},
-#endif
 	{"mvinchnstr",""},
-#if defined(__i386__)
-	{"mvinchnstr",""},
-#endif
 	{"mvinchstr",""},
-#if defined(__i386__)
-	{"mvinchstr",""},
-#endif
 	{"mvinnstr",""},
-#if defined(__i386__)
-	{"mvinnstr",""},
-#endif
 	{"mvinsch",""},
-#if defined(__i386__)
-	{"mvinsch",""},
-#endif
 	{"mvinsnstr",""},
-#if defined(__i386__)
-	{"mvinsnstr",""},
-#endif
 	{"mvinsstr",""},
-#if defined(__i386__)
-	{"mvinsstr",""},
-#endif
 	{"mvinstr",""},
-#if defined(__i386__)
-	{"mvinstr",""},
-#endif
 	{"mvprintw",""},
-#if defined(__i386__)
-	{"mvprintw",""},
-#endif
 	{"mvscanw",""},
-#if defined(__i386__)
-	{"mvscanw",""},
-#endif
 	{"mvvline",""},
-#if defined(__i386__)
-	{"mvvline",""},
-#endif
 	{"mvwaddch",""},
-#if defined(__i386__)
-	{"mvwaddch",""},
-#endif
 	{"mvwaddchnstr",""},
-#if defined(__i386__)
-	{"mvwaddchnstr",""},
-#endif
 	{"mvwaddchstr",""},
-#if defined(__i386__)
-	{"mvwaddchstr",""},
-#endif
 	{"mvwaddnstr",""},
-#if defined(__i386__)
-	{"mvwaddnstr",""},
-#endif
 	{"mvwaddstr",""},
-#if defined(__i386__)
-	{"mvwaddstr",""},
-#endif
 	{"mvwchgat",""},
-#if defined(__i386__)
-	{"mvwchgat",""},
-#endif
 	{"mvwdelch",""},
-#if defined(__i386__)
-	{"mvwdelch",""},
-#endif
 	{"mvwgetch",""},
-#if defined(__i386__)
-	{"mvwgetch",""},
-#endif
 	{"mvwgetnstr",""},
-#if defined(__i386__)
-	{"mvwgetnstr",""},
-#endif
 	{"mvwgetstr",""},
-#if defined(__i386__)
-	{"mvwgetstr",""},
-#endif
 	{"mvwhline",""},
-#if defined(__i386__)
-	{"mvwhline",""},
-#endif
 	{"mvwin",""},
-#if defined(__i386__)
-	{"mvwin",""},
-#endif
 	{"mvwinch",""},
-#if defined(__i386__)
-	{"mvwinch",""},
-#endif
 	{"mvwinchnstr",""},
-#if defined(__i386__)
-	{"mvwinchnstr",""},
-#endif
 	{"mvwinchstr",""},
-#if defined(__i386__)
-	{"mvwinchstr",""},
-#endif
 	{"mvwinnstr",""},
-#if defined(__i386__)
-	{"mvwinnstr",""},
-#endif
 	{"mvwinsch",""},
-#if defined(__i386__)
-	{"mvwinsch",""},
-#endif
 	{"mvwinsnstr",""},
-#if defined(__i386__)
-	{"mvwinsnstr",""},
-#endif
 	{"mvwinsstr",""},
-#if defined(__i386__)
-	{"mvwinsstr",""},
-#endif
 	{"mvwinstr",""},
-#if defined(__i386__)
-	{"mvwinstr",""},
-#endif
 	{"mvwprintw",""},
-#if defined(__i386__)
-	{"mvwprintw",""},
-#endif
 	{"mvwscanw",""},
-#if defined(__i386__)
-	{"mvwscanw",""},
-#endif
 	{"mvwvline",""},
-#if defined(__i386__)
-	{"mvwvline",""},
-#endif
 	{"napms",""},
-#if defined(__i386__)
-	{"napms",""},
-#endif
 	{"newpad",""},
-#if defined(__i386__)
-	{"newpad",""},
-#endif
 	{"newterm",""},
-#if defined(__i386__)
-	{"newterm",""},
-#endif
 	{"newwin",""},
-#if defined(__i386__)
-	{"newwin",""},
-#endif
 	{"nl",""},
-#if defined(__i386__)
-	{"nl",""},
-#endif
 	{"nocbreak",""},
-#if defined(__i386__)
-	{"nocbreak",""},
-#endif
 	{"nodelay",""},
-#if defined(__i386__)
-	{"nodelay",""},
-#endif
 	{"noecho",""},
-#if defined(__i386__)
-	{"noecho",""},
-#endif
 	{"nonl",""},
-#if defined(__i386__)
-	{"nonl",""},
-#endif
 	{"noqiflush",""},
-#if defined(__i386__)
-	{"noqiflush",""},
-#endif
 	{"noraw",""},
-#if defined(__i386__)
-	{"noraw",""},
-#endif
 	{"notimeout",""},
-#if defined(__i386__)
-	{"notimeout",""},
-#endif
 	{"overlay",""},
-#if defined(__i386__)
-	{"overlay",""},
-#endif
 	{"overwrite",""},
-#if defined(__i386__)
-	{"overwrite",""},
-#endif
 	{"pair_content",""},
-#if defined(__i386__)
-	{"pair_content",""},
-#endif
 	{"pechochar",""},
-#if defined(__i386__)
-	{"pechochar",""},
-#endif
 	{"pnoutrefresh",""},
-#if defined(__i386__)
-	{"pnoutrefresh",""},
-#endif
 	{"prefresh",""},
-#if defined(__i386__)
-	{"prefresh",""},
-#endif
 	{"printw",""},
-#if defined(__i386__)
-	{"printw",""},
-#endif
 	{"putp",""},
-#if defined(__i386__)
-	{"putp",""},
-#endif
 	{"putwin",""},
-#if defined(__i386__)
-	{"putwin",""},
-#endif
 	{"qiflush",""},
-#if defined(__i386__)
-	{"qiflush",""},
-#endif
 	{"raw",""},
-#if defined(__i386__)
-	{"raw",""},
-#endif
 	{"redrawwin",""},
-#if defined(__i386__)
-	{"redrawwin",""},
-#endif
 	{"refresh",""},
-#if defined(__i386__)
-	{"refresh",""},
-#endif
 	{"reset_prog_mode",""},
-#if defined(__i386__)
-	{"reset_prog_mode",""},
-#endif
 	{"reset_shell_mode",""},
-#if defined(__i386__)
-	{"reset_shell_mode",""},
-#endif
 	{"resetty",""},
-#if defined(__i386__)
-	{"resetty",""},
-#endif
 	{"ripoffline",""},
-#if defined(__i386__)
-	{"ripoffline",""},
-#endif
 	{"savetty",""},
-#if defined(__i386__)
-	{"savetty",""},
-#endif
 	{"scanw",""},
-#if defined(__i386__)
-	{"scanw",""},
-#endif
 	{"scr_dump",""},
-#if defined(__i386__)
-	{"scr_dump",""},
-#endif
 	{"scr_init",""},
-#if defined(__i386__)
-	{"scr_init",""},
-#endif
 	{"scr_restore",""},
-#if defined(__i386__)
-	{"scr_restore",""},
-#endif
 	{"scr_set",""},
-#if defined(__i386__)
-	{"scr_set",""},
-#endif
 	{"scrl",""},
-#if defined(__i386__)
-	{"scrl",""},
-#endif
 	{"scroll",""},
-#if defined(__i386__)
-	{"scroll",""},
-#endif
 	{"scrollok",""},
-#if defined(__i386__)
-	{"scrollok",""},
-#endif
 	{"set_term",""},
-#if defined(__i386__)
-	{"set_term",""},
-#endif
 	{"setscrreg",""},
-#if defined(__i386__)
-	{"setscrreg",""},
-#endif
-#if defined(__i386__)
 	{"setupterm",""},
-#endif
 	{"slk_attr_set",""},
-#if defined(__i386__)
-	{"slk_attr_set",""},
-#endif
 	{"slk_attroff",""},
-#if defined(__i386__)
-	{"slk_attroff",""},
-#endif
 	{"slk_attron",""},
-#if defined(__i386__)
-	{"slk_attron",""},
-#endif
 	{"slk_attrset",""},
-#if defined(__i386__)
-	{"slk_attrset",""},
-#endif
 	{"slk_clear",""},
-#if defined(__i386__)
-	{"slk_clear",""},
-#endif
 	{"slk_color",""},
-#if defined(__i386__)
-	{"slk_color",""},
-#endif
 	{"slk_init",""},
-#if defined(__i386__)
-	{"slk_init",""},
-#endif
 	{"slk_label",""},
-#if defined(__i386__)
-	{"slk_label",""},
-#endif
 	{"slk_noutrefresh",""},
-#if defined(__i386__)
-	{"slk_noutrefresh",""},
-#endif
 	{"slk_refresh",""},
-#if defined(__i386__)
-	{"slk_refresh",""},
-#endif
 	{"slk_restore",""},
-#if defined(__i386__)
-	{"slk_restore",""},
-#endif
 	{"slk_set",""},
-#if defined(__i386__)
-	{"slk_set",""},
-#endif
 	{"slk_touch",""},
-#if defined(__i386__)
-	{"slk_touch",""},
-#endif
 	{"standend",""},
-#if defined(__i386__)
-	{"standend",""},
-#endif
 	{"standout",""},
-#if defined(__i386__)
-	{"standout",""},
-#endif
 	{"start_color",""},
-#if defined(__i386__)
-	{"start_color",""},
-#endif
 	{"stdscr",""},
-#if defined(__i386__)
-	{"stdscr",""},
-#endif
 	{"subpad",""},
-#if defined(__i386__)
-	{"subpad",""},
-#endif
 	{"subwin",""},
-#if defined(__i386__)
-	{"subwin",""},
-#endif
 	{"syncok",""},
-#if defined(__i386__)
-	{"syncok",""},
-#endif
 	{"termattrs",""},
-#if defined(__i386__)
-	{"termattrs",""},
-#endif
 	{"termname",""},
-#if defined(__i386__)
-	{"termname",""},
-#endif
 	{"tgetent",""},
-#if defined(__i386__)
-	{"tgetent",""},
-#endif
 	{"tgetflag",""},
-#if defined(__i386__)
-	{"tgetflag",""},
-#endif
 	{"tgetnum",""},
-#if defined(__i386__)
-	{"tgetnum",""},
-#endif
 	{"tgetstr",""},
-#if defined(__i386__)
-	{"tgetstr",""},
-#endif
 	{"tgoto",""},
-#if defined(__i386__)
-	{"tgoto",""},
-#endif
 	{"tigetflag",""},
-#if defined(__i386__)
-	{"tigetflag",""},
-#endif
 	{"tigetnum",""},
-#if defined(__i386__)
-	{"tigetnum",""},
-#endif
 	{"tigetstr",""},
-#if defined(__i386__)
-	{"tigetstr",""},
-#endif
 	{"timeout",""},
-#if defined(__i386__)
-	{"timeout",""},
-#endif
-#if defined(__i386__)
 	{"touchline",""},
-#endif
-#if defined(__i386__)
 	{"touchwin",""},
-#endif
 	{"tparm",""},
-#if defined(__i386__)
-	{"tparm",""},
-#endif
 	{"tputs",""},
-#if defined(__i386__)
-	{"tputs",""},
-#endif
 	{"typeahead",""},
-#if defined(__i386__)
-	{"typeahead",""},
-#endif
 	{"ungetch",""},
-#if defined(__i386__)
-	{"ungetch",""},
-#endif
 	{"untouchwin",""},
-#if defined(__i386__)
-	{"untouchwin",""},
-#endif
 	{"use_env",""},
-#if defined(__i386__)
-	{"use_env",""},
-#endif
 	{"vidattr",""},
-#if defined(__i386__)
-	{"vidattr",""},
-#endif
 	{"vidputs",""},
-#if defined(__i386__)
-	{"vidputs",""},
-#endif
 	{"vline",""},
-#if defined(__i386__)
-	{"vline",""},
-#endif
 	{"vw_printw",""},
-#if defined(__i386__)
-	{"vw_printw",""},
-#endif
 	{"vw_scanw",""},
-#if defined(__i386__)
-	{"vw_scanw",""},
-#endif
 	{"vwprintw",""},
-#if defined(__i386__)
-	{"vwprintw",""},
-#endif
 	{"vwscanw",""},
-#if defined(__i386__)
-	{"vwscanw",""},
-#endif
 	{"waddch",""},
-#if defined(__i386__)
-	{"waddch",""},
-#endif
 	{"waddchnstr",""},
-#if defined(__i386__)
-	{"waddchnstr",""},
-#endif
 	{"waddchstr",""},
-#if defined(__i386__)
-	{"waddchstr",""},
-#endif
 	{"waddnstr",""},
-#if defined(__i386__)
-	{"waddnstr",""},
-#endif
 	{"waddstr",""},
-#if defined(__i386__)
-	{"waddstr",""},
-#endif
 	{"wattr_get",""},
-#if defined(__i386__)
-	{"wattr_get",""},
-#endif
 	{"wattr_off",""},
-#if defined(__i386__)
-	{"wattr_off",""},
-#endif
 	{"wattr_on",""},
-#if defined(__i386__)
-	{"wattr_on",""},
-#endif
 	{"wattr_set",""},
-#if defined(__i386__)
-	{"wattr_set",""},
-#endif
 	{"wattroff",""},
-#if defined(__i386__)
-	{"wattroff",""},
-#endif
 	{"wattron",""},
-#if defined(__i386__)
-	{"wattron",""},
-#endif
 	{"wattrset",""},
-#if defined(__i386__)
-	{"wattrset",""},
-#endif
 	{"wbkgd",""},
-#if defined(__i386__)
-	{"wbkgd",""},
-#endif
 	{"wbkgdset",""},
-#if defined(__i386__)
-	{"wbkgdset",""},
-#endif
 	{"wborder",""},
-#if defined(__i386__)
-	{"wborder",""},
-#endif
 	{"wchgat",""},
-#if defined(__i386__)
-	{"wchgat",""},
-#endif
 	{"wclear",""},
-#if defined(__i386__)
-	{"wclear",""},
-#endif
 	{"wclrtobot",""},
-#if defined(__i386__)
-	{"wclrtobot",""},
-#endif
 	{"wclrtoeol",""},
-#if defined(__i386__)
-	{"wclrtoeol",""},
-#endif
 	{"wcolor_set",""},
-#if defined(__i386__)
-	{"wcolor_set",""},
-#endif
 	{"wcursyncup",""},
-#if defined(__i386__)
-	{"wcursyncup",""},
-#endif
 	{"wdelch",""},
-#if defined(__i386__)
-	{"wdelch",""},
-#endif
 	{"wdeleteln",""},
-#if defined(__i386__)
-	{"wdeleteln",""},
-#endif
 	{"wechochar",""},
-#if defined(__i386__)
-	{"wechochar",""},
-#endif
 	{"werase",""},
-#if defined(__i386__)
-	{"werase",""},
-#endif
 	{"wgetch",""},
-#if defined(__i386__)
-	{"wgetch",""},
-#endif
 	{"wgetnstr",""},
-#if defined(__i386__)
-	{"wgetnstr",""},
-#endif
 	{"wgetstr",""},
-#if defined(__i386__)
-	{"wgetstr",""},
-#endif
 	{"whline",""},
-#if defined(__i386__)
-	{"whline",""},
-#endif
 	{"winch",""},
-#if defined(__i386__)
-	{"winch",""},
-#endif
 	{"winchnstr",""},
-#if defined(__i386__)
-	{"winchnstr",""},
-#endif
 	{"winchstr",""},
-#if defined(__i386__)
-	{"winchstr",""},
-#endif
 	{"winnstr",""},
-#if defined(__i386__)
-	{"winnstr",""},
-#endif
 	{"winsch",""},
-#if defined(__i386__)
-	{"winsch",""},
-#endif
 	{"winsdelln",""},
-#if defined(__i386__)
-	{"winsdelln",""},
-#endif
 	{"winsertln",""},
-#if defined(__i386__)
-	{"winsertln",""},
-#endif
 	{"winsnstr",""},
-#if defined(__i386__)
-	{"winsnstr",""},
-#endif
 	{"winsstr",""},
-#if defined(__i386__)
-	{"winsstr",""},
-#endif
 	{"winstr",""},
-#if defined(__i386__)
-	{"winstr",""},
-#endif
 	{"wmove",""},
-#if defined(__i386__)
-	{"wmove",""},
-#endif
 	{"wnoutrefresh",""},
-#if defined(__i386__)
-	{"wnoutrefresh",""},
-#endif
 	{"wprintw",""},
-#if defined(__i386__)
-	{"wprintw",""},
-#endif
 	{"wredrawln",""},
-#if defined(__i386__)
-	{"wredrawln",""},
-#endif
 	{"wrefresh",""},
-#if defined(__i386__)
-	{"wrefresh",""},
-#endif
 	{"wscanw",""},
-#if defined(__i386__)
-	{"wscanw",""},
-#endif
 	{"wscrl",""},
-#if defined(__i386__)
-	{"wscrl",""},
-#endif
 	{"wsetscrreg",""},
-#if defined(__i386__)
-	{"wsetscrreg",""},
-#endif
 	{"wstandend",""},
-#if defined(__i386__)
-	{"wstandend",""},
-#endif
 	{"wstandout",""},
-#if defined(__i386__)
-	{"wstandout",""},
-#endif
 	{"wsyncdown",""},
-#if defined(__i386__)
-	{"wsyncdown",""},
-#endif
 	{"wsyncup",""},
-#if defined(__i386__)
-	{"wsyncup",""},
-#endif
 	{"wtimeout",""},
-#if defined(__i386__)
-	{"wtimeout",""},
-#endif
 	{"wtouchln",""},
-#if defined(__i386__)
-	{"wtouchln",""},
-#endif
 	{"wvline",""},
-#if defined(__i386__)
-	{"wvline",""},
-#endif
 	{0,0}};
