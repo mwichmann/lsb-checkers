fd48c947e3875a2d695761027a83bec5287af49e
cyeoh <>
2002-10-30 05:37:38 -0400

    Changes for ppc32 Asymbol update

--- elfchk/dtneeded.c
+++ elfchk/dtneeded.c
@@ -1,104 +1,104 @@
 /* Generated file - Do Not Edit */
 char *DtNeeded[] = {
-#if defined(__powerpc64__)
+#if __powerpc64__
 	"/lib/ld-lsb-64.so.1",
 #endif /* __powerpc64__ */
-#if defined(__ia64__)
+#if __ia64__
 	"/lib/ld-lsb-ia64.so.1",
 #endif /* __ia64__ */
-#if defined(__s390__)
+#if __s390__
 	"/lib/ld-lsb-s390.so.1",
 #endif /* __s390__ */
-#if defined(__x86_64__)
+#if __x86_64__
 	"/lib/ld-lsb-x86_64.so.1",
 #endif /* __x86_64__ */
-#if defined(__i386__)
+#if __i386__
 	"/lib/ld-lsb.so.1",
 #endif /* __i386__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	"/lib/ld-lsb.so.1",
-#endif /* __powerpc__ */
-#if defined(__i386__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __i386__
 	"libc.so.6",
 #endif /* __i386__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	"libc.so.6",
-#endif /* __powerpc__ */
-#if defined(__powerpc64__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __powerpc64__
 	"libc.so.6",
 #endif /* __powerpc64__ */
-#if defined(__s390__)
+#if __s390__
 	"libc.so.6",
 #endif /* __s390__ */
-#if defined(__ia64__)
+#if __ia64__
 	"libc.so.6.1",
 #endif /* __ia64__ */
 	"libcrypt.so.1",
-#if defined(__i386__)
+#if __i386__
 	"libcrypt.so.1",
 #endif /* __i386__ */
-#if defined(__ia64__)
+#if __ia64__
 	"libcrypt.so.1",
 #endif /* __ia64__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	"libcrypt.so.1",
-#endif /* __powerpc__ */
-#if defined(__powerpc64__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __powerpc64__
 	"libcrypt.so.1",
 #endif /* __powerpc64__ */
-#if defined(__s390__)
+#if __s390__
 	"libcrypt.so.1",
 #endif /* __s390__ */
 	"libdl.so.2",
-#if defined(__i386__)
+#if __i386__
 	"libdl.so.2",
 #endif /* __i386__ */
-#if defined(__ia64__)
+#if __ia64__
 	"libdl.so.2",
 #endif /* __ia64__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	"libdl.so.2",
-#endif /* __powerpc__ */
-#if defined(__powerpc64__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __powerpc64__
 	"libdl.so.2",
 #endif /* __powerpc64__ */
-#if defined(__s390__)
+#if __s390__
 	"libdl.so.2",
 #endif /* __s390__ */
 	"libgcc_s.so.1",
 	"libGL.so.1",
 	"libICE.so.6",
-#if defined(__i386__)
+#if __i386__
 	"libm.so.6",
 #endif /* __i386__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	"libm.so.6",
-#endif /* __powerpc__ */
-#if defined(__powerpc64__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __powerpc64__
 	"libm.so.6",
 #endif /* __powerpc64__ */
-#if defined(__s390__)
+#if __s390__
 	"libm.so.6",
 #endif /* __s390__ */
-#if defined(__ia64__)
+#if __ia64__
 	"libm.so.6.1",
 #endif /* __ia64__ */
 	"libncurses.so.5",
 	"libpam.so.0",
 	"libpthread.so.0",
-#if defined(__i386__)
+#if __i386__
 	"libpthread.so.0",
 #endif /* __i386__ */
-#if defined(__ia64__)
+#if __ia64__
 	"libpthread.so.0",
 #endif /* __ia64__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	"libpthread.so.0",
-#endif /* __powerpc__ */
-#if defined(__powerpc64__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __powerpc64__
 	"libpthread.so.0",
 #endif /* __powerpc64__ */
-#if defined(__s390__)
+#if __s390__
 	"libpthread.so.0",
 #endif /* __s390__ */
 	"libSM.so.6",
--- elfchk/sectinfo.c
+++ elfchk/sectinfo.c
@@ -19,32 +19,32 @@ struct SectionInfo SectionInfo[] = {
 	{".gnu.version",SHT_GNU_versym,SHF_ALLOC,checkGNU_versym},
 	{".gnu.version_d",SHT_GNU_verdef,SHF_ALLOC,checkGNU_verdef},
 	{".gnu.version_r",SHT_GNU_verneed,SHF_ALLOC,checkGNU_verneed},
-#if defined(__i386__)
+#if __i386__
 	{".got",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
 #endif /* __i386__ */
-#if defined(__ia64__)
+#if __ia64__
 	{".got",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE+SHF_IA_64_SHORT,checkPROGBITS},
 #endif /* __ia64__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	{".got",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE+SHF_EXECINSTR,checkPROGBITS},
-#endif /* __powerpc__ */
-#if defined(__s390__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __s390__
 	{".got",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
 #endif /* __s390__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	{".got2",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
-#endif /* __powerpc__ */
+#endif /* __powerpc__ && !__powerpc64__ */
 	{".hash",SHT_HASH,SHF_ALLOC,checkHASH},
-#if defined(__ia64__)
+#if __ia64__
 	{".IA_64.archext",SHT_IA_64_EXT,0,checkIA_64_EXT},
 #endif /* __ia64__ */
-#if defined(__ia64__)
+#if __ia64__
 	{".IA_64.pltoff",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE+SHF_IA_64_SHORT,checkPROGBITS},
 #endif /* __ia64__ */
-#if defined(__ia64__)
+#if __ia64__
 	{".IA_64.unwind",SHT_IA_64_UNWIND,SHF_ALLOC+SHF_LINK_ORDER,checkIA_64_UNWIND},
 #endif /* __ia64__ */
-#if defined(__ia64__)
+#if __ia64__
 	{".IA_64.unwind_info",SHT_PROGBITS,SHF_ALLOC,checkPROGBITS},
 #endif /* __ia64__ */
 	{".init",SHT_PROGBITS,SHF_ALLOC+SHF_EXECINSTR,checkPROGBITS},
@@ -54,73 +54,79 @@ struct SectionInfo SectionInfo[] = {
 	{".line",SHT_PROGBITS,0,checkPROGBITS},
 	{".note",SHT_NOTE,0,checkNOTE},
 	{".note.ABI-tag",SHT_NOTE,SHF_ALLOC,checkNOTE},
-#if defined(__i386__)
+#if __i386__
 	{".plt",SHT_PROGBITS,SHF_ALLOC+SHF_EXECINSTR,checkPROGBITS},
 #endif /* __i386__ */
-#if defined(__ia64__)
+#if __ia64__
 	{".plt",SHT_PROGBITS,SHF_ALLOC+SHF_EXECINSTR,checkPROGBITS},
 #endif /* __ia64__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	{".plt",SHT_NOBITS,SHF_ALLOC+SHF_WRITE+SHF_EXECINSTR,checkNOBITS},
-#endif /* __powerpc__ */
-#if defined(__s390__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __s390__
 	{".plt",SHT_PROGBITS,SHF_ALLOC+SHF_EXECINSTR,checkPROGBITS},
 #endif /* __s390__ */
 	{".preinit_array",SHT_PREINIT_ARRAY,SHF_ALLOC+SHF_WRITE,checkPREINIT_ARRAY},
-#if defined(__i386__)
+#if __i386__
 	{".rel.bss",SHT_REL,SHF_ALLOC,checkREL},
 #endif /* __i386__ */
-#if defined(__i386__)
+#if __i386__
 	{".rel.data",SHT_REL,0,checkREL},
 #endif /* __i386__ */
-#if defined(__i386__)
+#if __i386__
 	{".rel.dyn",SHT_REL,SHF_ALLOC,checkREL},
 #endif /* __i386__ */
-#if defined(__i386__)
+#if __i386__
 	{".rel.got",SHT_REL,SHF_ALLOC,checkREL},
 #endif /* __i386__ */
-#if defined(__i386__)
+#if __i386__
 	{".rel.plt",SHT_REL,SHF_ALLOC,checkREL},
 #endif /* __i386__ */
-#if defined(__i386__)
+#if __i386__
 	{".rel.text",SHT_REL,0,checkREL},
 #endif /* __i386__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	{".rela.bss",SHT_RELA,SHF_ALLOC,checkRELA},
-#endif /* __powerpc__ */
-#if defined(__ia64__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __ia64__
 	{".rela.dyn",SHT_RELA,SHF_ALLOC,checkRELA},
 #endif /* __ia64__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	{".rela.dyn",SHT_RELA,SHF_ALLOC,checkRELA},
-#endif /* __powerpc__ */
-#if defined(__powerpc__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __s390__
+	{".rela.dyn",SHT_RELA,SHF_ALLOC,checkRELA},
+#endif /* __s390__ */
+#if __powerpc__ && !__powerpc64__
 	{".rela.got",SHT_RELA,SHF_ALLOC,checkRELA},
-#endif /* __powerpc__ */
-#if defined(__powerpc__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __powerpc__ && !__powerpc64__
 	{".rela.got2",SHT_RELA,SHF_ALLOC,checkRELA},
-#endif /* __powerpc__ */
-#if defined(__ia64__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __ia64__
 	{".rela.IA_64.pltoff",SHT_RELA,SHF_ALLOC,checkRELA},
 #endif /* __ia64__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	{".rela.plt",SHT_RELA,SHF_ALLOC,checkRELA},
-#endif /* __powerpc__ */
-#if defined(__powerpc__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __s390__
+	{".rela.plt",SHT_RELA,SHF_ALLOC,checkRELA},
+#endif /* __s390__ */
+#if __powerpc__ && !__powerpc64__
 	{".rela.sbss",SHT_RELA,SHF_ALLOC,checkRELA},
-#endif /* __powerpc__ */
+#endif /* __powerpc__ && !__powerpc64__ */
 	{".rodata",SHT_PROGBITS,SHF_ALLOC,checkPROGBITS},
 	{".rodata1",SHT_PROGBITS,SHF_ALLOC,checkPROGBITS},
-#if defined(__ia64__)
+#if __ia64__
 	{".sbss",SHT_NOBITS,SHF_ALLOC+SHF_WRITE+SHF_IA_64_SHORT,checkNOBITS},
 #endif /* __ia64__ */
-#if defined(__ia64__)
+#if __ia64__
 	{".sdata",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE+SHF_IA_64_SHORT,checkPROGBITS},
 #endif /* __ia64__ */
-#if defined(__powerpc__)
+#if __powerpc__ && !__powerpc64__
 	{".sdata",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE,checkPROGBITS},
-#endif /* __powerpc__ */
-#if defined(__ia64__)
+#endif /* __powerpc__ && !__powerpc64__ */
+#if __ia64__
 	{".sdata1",SHT_PROGBITS,SHF_ALLOC+SHF_WRITE+SHF_IA_64_SHORT,checkPROGBITS},
 #endif /* __ia64__ */
 	{".shstrtab",SHT_STRTAB,0,checkSTRTAB},
