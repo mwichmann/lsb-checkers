70d2522a8bb3936bce63b175cea35984f25da04d
anderson <anderson>
2002-11-19 19:53:07 -0400

    Be more flexible in Program HEader type values

--- elfchk/proghdr.h
+++ elfchk/proghdr.h
@@ -17,7 +17,8 @@ extern int checkPT_INTERP(ElfFile *, Elf32_Phdr *, struct tetj_handle *);
 extern int checkPT_NOTE(ElfFile *, Elf32_Phdr *, struct tetj_handle *);
 extern int checkPT_SHLIB(ElfFile *, Elf32_Phdr *, struct tetj_handle *);
 extern int checkPT_PHDR(ElfFile *, Elf32_Phdr *, struct tetj_handle *);
-extern int checkPT_NUM(ElfFile *, Elf32_Phdr *, struct tetj_handle *);
+extern int checkPT_TLS(ElfFile *, Elf32_Phdr *, struct tetj_handle *);
+extern int checkPT_GNU_EH_FRAME(ElfFile *, Elf32_Phdr *, struct tetj_handle *);
 extern void checkElfproghdr(int index, ElfFile *file1, struct tetj_handle *);
 
 #endif /* _PROGHDR_H */
