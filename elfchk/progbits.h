a76f5304ed39ed52dbbc6e4ad19fd9463c632c2a
anderson <anderson>
2002-11-20 17:12:32 -0400

    Initial code for parsing the .eh_frame_hdr data

--- elfchk/progbits.h
+++ elfchk/progbits.h
@@ -12,6 +12,7 @@ typedef int (*ProgBitsFcn)(ElfFile *, Elf32_Shdr *, struct tetj_handle *);
 int checkPROGBITS_data(ElfFile *, Elf32_Shdr *, struct tetj_handle *);
 int checkPROGBITS_data1(ElfFile *, Elf32_Shdr *, struct tetj_handle *);
 int checkPROGBITS_eh_frame(ElfFile *, Elf32_Shdr *, struct tetj_handle *);
+int checkPROGBITS_eh_frame_hdr(ElfFile *, Elf32_Shdr *, struct tetj_handle *);
 int checkPROGBITS_interp(ElfFile *, Elf32_Shdr *, struct tetj_handle *);
 int checkPROGBITS_rodata(ElfFile *, Elf32_Shdr *, struct tetj_handle *);
 int checkPROGBITS_rodata1(ElfFile *, Elf32_Shdr *, struct tetj_handle *);
