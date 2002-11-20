0ec816c923e7e24ce35d64c4e262c3d53525b661
anderson <>
2002-11-19 21:37:18 -0400

    Hide one more debug message behind ELFCHK_DEBUG

--- elfchk/progbits.c
+++ elfchk/progbits.c
@@ -46,7 +46,9 @@ return 0;
 int
 checkPROGBITS_eh_frame(ElfFile *file1, Elf32_Shdr *hdr1, struct tetj_handle *journal)
 {
-fprintf(stderr,"checking .eh_frame %x bytes at %x\n",hdr1->sh_size, hdr1->sh_offset);
+if( elfchk_debug&DEBUG_SECTION_CONTENTS )
+	fprintf(stderr,"checking .eh_frame %x bytes at %x\n",
+				hdr1->sh_size, hdr1->sh_offset);
 
 /*
  * We should loop over this until the entire section has been used up, but
