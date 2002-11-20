290b7c54621bc5f9d03b5c83962bc3922c083da7
anderson <anderson>
2002-11-20 15:13:36 -0400

    Handle the alignment requirments of note elements

--- elfchk/note.c
+++ elfchk/note.c
@@ -13,22 +13,36 @@ check_NOTE(ElfFile *file, unsigned char *notes, int length, struct tetj_handle *
 {
 Elf32_Nhdr	*note;
 char	*name,*desc;
+int	curlen=0;
 
 note=(Elf32_Nhdr *)notes;
-while(length>0) {
+while(curlen<length) {
 	//fprintf(stderr,"note %x length %x\n", note, length);
 	//fprintf(stderr,"n_namesz %d\t", note->n_namesz );
 	name=(char *)(note+1);
+	curlen+=sizeof(Elf32_Nhdr);
 	//fprintf(stderr,"n_descsz %d\t", note->n_descsz );
 	desc=(name+note->n_namesz);
+	curlen+=note->n_namesz;
+	/* Pad if needed */
+	switch(curlen%sizeof(Elf32_Off)) {
+		case 1:	curlen++;
+		case 2:	curlen++;
+		case 3:	curlen++;
+		}
 	//fprintf(stderr,"n_type %d\n", note->n_type );
 	//fprintf(stderr,"name: %s\n", name);
 	//fprintf(stderr,"desc:" );
 	//dumpbytes(desc,note->n_descsz);
-	length-=sizeof(Elf32_Nhdr);
-	length-=note->n_namesz;
-	length-=note->n_descsz;
-	note=(Elf32_Nhdr *)(((char *)note)+sizeof(Elf32_Nhdr)+note->n_namesz+note->n_descsz);
+	curlen+=note->n_descsz;
+	/* Pad if needed */
+	switch(curlen%sizeof(Elf32_Off)) {
+		case 1:	curlen++;
+		case 2:	curlen++;
+		case 3:	curlen++;
+		}
+			
+	note=(Elf32_Nhdr *)(((char *)notes)+curlen);
 	}
 
 return 1;
