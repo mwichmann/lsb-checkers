9edb58472092010ecff0193aa38cdaa06c7e79c5
anderson <>
2002-11-26 01:22:04 -0400

    Spelling is not a job requirement

--- elfchk/symvers.c
+++ elfchk/symvers.c
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright (c) 2001-2002 The Free Standards Group Inc
- * Copyright (c) 2000-2002 Stuart Anderson (anderson@freestnadards.org)
+ * Copyright (c) 2000-2002 Stuart Anderson (anderson@freestandards.org)
  * Copyright (c) 2002 Chris Yeoh (yeohc@au.ibm.com)
  *
  */
