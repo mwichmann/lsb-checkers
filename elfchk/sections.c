f48789013ee7434f654d75292d6329a9d9864fc4
cyeoh <>
2002-12-09 02:10:32 -0400

    Only display warning about sections not being checked if VERBOSE is   defined Return 1 (success) if checking for section has not yet been implemented

--- elfchk/sections.c
+++ elfchk/sections.c
@@ -106,9 +106,11 @@ checkPROGBITS(ElfFile *file1, Elf32_Shdr *hdr1, struct tetj_handle *journal)
   		return ProgbitsInfo[i].func( file1, hdr1, journal );
 	}
   }
+#ifdef VERBOSE
 fprintf(stderr, "Contents of section %s not checked.\n",
 				ElfGetString(file1, hdr1->sh_name));
-  return 0;
+#endif
+  return 1;
 }
 
 int
