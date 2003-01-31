ee6e9eea14488037b4d10b3456bd1cf114bb88a3
cyeoh <cyeoh>
2003-01-31 06:05:19 -0400

    Fix use of initialized variable in check_CIE Add check for zero terminating frames which we shouldn't parse Add whitespace for clarity

--- elfchk/dwarf.c
+++ elfchk/dwarf.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include "elfchk.h"
 #include "dwarf.h"
 
@@ -38,8 +39,8 @@ read_leb128 (data, length_return, sign)
 
 int check_CFI(unsigned char *ptr,int length)
 {
-int	numused,used=0;
-int	tmp;
+        int	numused,used=0;
+        int	tmp;
 
 /* XXXSTU - Need to check the values read in each record */
 
@@ -49,232 +50,302 @@ fprintf(stderr,"\nCFI found at %x", ptr );
 dumpbytes(ptr,8);
 */
 
-used++; /* The op code */
-switch( *ptr ) {
+        used++; /* The op code */
+        switch( *ptr ) {
 	case 0x00: /* DW_CFA_nop */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if (elfchk_debug & DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,"DW_CFA_nop\n" );
 		}
 		break;
+
 	case 0x02: /* DW_CFA_advance_loc1 */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if (elfchk_debug & DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,"DW_CFA_advance_loc1\n" );
-			}
-		tmp=*(int *)(ptr);ptr+=sizeof(int);used+=sizeof(int);
+                }
+		tmp = *(int *)(ptr);
+                ptr += sizeof(int);
+                used += sizeof(int);
 		break;
+
 	case 0x03: /* DW_CFA_advance_loc2 */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if (elfchk_debug & DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,"DW_CFA_advance_loc2\n" );
-			}
-		tmp=*(int *)(ptr);ptr+=sizeof(int);used+=sizeof(int);
+                }
+		tmp =* (int *)(ptr);
+                ptr += sizeof(int);
+                used += sizeof(int);
 		break;
+
 	case 0x04: /* DW_CFA_advance_loc4 */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if (elfchk_debug & DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,"DW_CFA_advance_loc4\n" );
-			}
-		tmp=*(int *)(ptr);ptr+=sizeof(int);used+=sizeof(int);
+                }
+		tmp =* (int *)(ptr);
+                ptr += sizeof(int);
+                used += sizeof(int);
 		break;
+
 	case 0x05: /* DW_CFA_offset_extended */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if (elfchk_debug & DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,"DW_CFA_offset_extended\n" );
-			}
-		tmp=read_leb128(ptr,&numused,1);ptr+=numused;used+=numused;
-		tmp=read_leb128(ptr,&numused,1);ptr+=numused;used+=numused;
+                }
+		tmp = read_leb128(ptr,&numused,1);
+                ptr+=numused;
+                used+=numused;
+
+		tmp = read_leb128(ptr,&numused,1);
+                ptr += numused;
+                used += numused;
 		break;
+
 	case 0x0D: /* DW_CFA_def_cfa_register */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if (elfchk_debug & DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,"DW_CFA_def_cfa_register\n" );
-			}
-		tmp=read_leb128(ptr,&numused,1);ptr+=numused;used+=numused;
+                }
+		tmp = read_leb128(ptr,&numused,1);
+                ptr += numused;
+                used += numused;
 		break;
+
 	case 0x0E: /* DW_CFA_def_cfa_offset */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if (elfchk_debug&DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,"DW_CFA_def_cfa_offset\n" );
-			}
-		tmp=read_leb128(ptr,&numused,1);ptr+=numused;used+=numused;
+                }
+		tmp = read_leb128(ptr,&numused,1);
+                ptr += numused;
+                used += numused;
 		break;
+
 	case 0x0C: /* DW_CFA_def_cfa */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if (elfchk_debug&DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,"DW_CFA_def_cfa\n" );
-			}
-		tmp=read_leb128(ptr,&numused,0);ptr+=numused;used+=numused;
-		tmp=read_leb128(ptr,&numused,0);ptr+=numused;used+=numused;
+                }
+		tmp = read_leb128(ptr,&numused,0);
+                ptr += numused;
+                used += numused;
+
+		tmp = read_leb128(ptr,&numused,0);
+                ptr += numused;
+                used += numused;
 		break;
+
 	case 0x85: /* DW_CFA_offset|DW_CFA_offset_extended */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if (elfchk_debug&DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,
 				"DW_CFA_offset|DW_CFA_offset_extended\n" );
-			}
-		tmp=read_leb128(ptr,&numused,0);ptr+=numused;used+=numused;
+                }
+		tmp = read_leb128(ptr,&numused,0);
+                ptr += numused;
+                used += numused;
 		break;
+
 	case 0x88: /* DW_CFA_offset|DW_CFA_same_value */
-		if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
+		if(elfchk_debug&DEBUG_DWARF_CONTENTS) {
 			fprintf(stderr,"DW_CFA_offset|DW_CFA_same_value\n" );
-			}
-		tmp=read_leb128(ptr,&numused,0);ptr+=numused;used+=numused;
+                }
+		tmp = read_leb128(ptr,&numused,0);
+                ptr += numused;
+                used += numused;
 		break;
+
 	default:
 		fprintf(stderr, "********** Unexpected CFI opcode %x **\n",
-				*ptr);
-	}
+                        *ptr);
+        }
 
-return used;
+        return used;
+
 }
 
 int check_FDE(unsigned char *ptr,int length)
 {
-int	numused;
-int	used=0;
-FDEFrameHeader	fdeimage;
-unsigned char *endptr=ptr;
+        int	numused;
+        int	used=0;
+        FDEFrameHeader	fdeimage;
+        unsigned char *endptr=ptr;
 
 /*
 fprintf(stderr,"FDE found at %x\n", ptr );
 dumpbytes(ptr,length);
 */
 
-fdeimage.length=*(int *)ptr;ptr+=4;length-=4;used+=4;
-fdeimage.CIE_pointer=*(int *)ptr;ptr+=4;length-=4;used+=4;
-fdeimage.initial_location=*(int **)ptr;ptr+=4;length-=4;used+=4;
-fdeimage.address_range=*(int *)ptr;ptr+=4;length-=4;used+=4;
-fdeimage.address_range=read_leb128(ptr,&numused,1);ptr+=numused;length-=numused;used+=4;
+        fdeimage.length = *(int *)ptr;
+        ptr += 4;
+        length -= 4;
+        used += 4;
 
-if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
-	fprintf(stderr,"length: %x\n", fdeimage.length);
-	fprintf(stderr,"CIE_pointer: %x\n", fdeimage.CIE_pointer);
-	fprintf(stderr,"initial_location: %x\n", fdeimage.initial_location);
-	fprintf(stderr,"address_range: %x\n", fdeimage.address_range);
+        fdeimage.CIE_pointer = *(int *)ptr;
+        ptr += 4;
+        length -= 4;
+        used += 4;
+
+        fdeimage.initial_location = *(int **)ptr;
+        ptr+=4;
+        length-=4;
+        used+=4;
+        
+        fdeimage.address_range = *(int *)ptr;
+        ptr += 4;
+        length -= 4;
+        used += 4;
+        
+        fdeimage.address_range = read_leb128(ptr,&numused,1);
+        ptr += numused;
+        length -= numused;
+        used += 4;
+
+        if(elfchk_debug & DEBUG_DWARF_CONTENTS) {
+                fprintf(stderr,"length: %x\n", fdeimage.length);
+                fprintf(stderr,"CIE_pointer: %x\n", fdeimage.CIE_pointer);
+                fprintf(stderr,"initial_location: %p\n", 
+                        fdeimage.initial_location);
+                fprintf(stderr,"address_range: %x\n", fdeimage.address_range);
 	}
 
-endptr+=fdeimage.length;
-while(ptr<endptr)
-	ptr+=check_CFI(ptr,length);
+        endptr += fdeimage.length;
+        while(ptr<endptr)
+                ptr += check_CFI(ptr,length);
 
-return fdeimage.length+4; /* length+sizeof(length) */
+        return fdeimage.length + 4; /* length+sizeof(length) */
 }
 
 int check_CIE(unsigned char *ptr,int length)
 {
-int     used,numused;
-CIEFrameImage *frameimg;
-CIEFrame frame;
+        int     used,numused;
+        CIEFrameImage *frameimg;
+        CIEFrame frame;
 
-frameimg=(CIEFrameImage *)ptr;
+        frameimg = (CIEFrameImage *)ptr;
 
 /*
 fprintf(stderr,"CIE found at %x\n", ptr );
 dumpbytes(frameimg,frameimg->length);
 */
+        /* If the length of the frame is a zero terminating
+           frame and we shouldn't attempt to parse it */
+        if (frameimg->length==0)
+                return 0;
+        
+        frame.length = frameimg->length;
+        frame.cie = frameimg->cie;
+        frame.version = frameimg->version;
+        frame.augmentation = frameimg->augmentation;
 
-frame.length=frameimg->length;
-frame.cie=frameimg->cie;
-frame.version=frameimg->version;
-frame.augmentation=frameimg->augmentation;
-
-if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
-	fprintf(stderr,"length: %x\n", frame.length);
-	fprintf(stderr,"cie: %x\n", frame.cie);
-	fprintf(stderr,"ver: %x\n", frame.version);
-	fprintf(stderr,"aug: %s\n", frame.augmentation);
+        if (elfchk_debug & DEBUG_DWARF_CONTENTS) {
+                fprintf(stderr,"length: %x\n", frame.length);
+                fprintf(stderr,"cie: %x\n", frame.cie);
+                fprintf(stderr,"ver: %x\n", frame.version);
+                fprintf(stderr,"aug: %s\n", frame.augmentation);
 	}
 
-ptr=(unsigned char *)(&frameimg->augmentation[0])+strlen(frameimg->augmentation)+1;
+        ptr = (unsigned char *)(frameimg->augmentation)
+                + strlen(frameimg->augmentation) + 1;
 
-frame.code_alignment_factor=read_leb128(ptr,&numused,0);ptr+=numused;
-frame.data_alignment_factor=read_leb128(ptr,&numused,1);ptr+=numused;
-frame.return_address_register=*ptr++;
+        frame.code_alignment_factor = read_leb128(ptr, &numused, 0);
+        ptr += numused;
 
-if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
-	fprintf(stderr,"code_af: %d\n", frame.code_alignment_factor);
-	fprintf(stderr,"data_af: %d\n", frame.data_alignment_factor);
-	fprintf(stderr,"return_address_register: %x\n",
-		frame.return_address_register);
+        frame.data_alignment_factor = read_leb128(ptr,&numused,1);
+        ptr+=numused;
+
+        frame.return_address_register = *ptr++;
+
+        if(elfchk_debug & DEBUG_DWARF_CONTENTS) {
+                fprintf(stderr,"code_af: %d\n", frame.code_alignment_factor);
+                fprintf(stderr,"data_af: %d\n", frame.data_alignment_factor);
+                fprintf(stderr,"return_address_register: %x\n",
+                        frame.return_address_register);
 	}
 
-/* Get the info related with 'z' */
-frame.augmentation_len=read_leb128(ptr,&numused,0);ptr+=numused;
-if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
-	fprintf(stderr,"augmentation_len: %x\n", frame.augmentation_len);
+        /* Get the info related with 'z' */
+        frame.augmentation_len = read_leb128(ptr,&numused,0);
+        ptr += numused;
+        if (elfchk_debug & DEBUG_DWARF_CONTENTS) {
+                fprintf(stderr,"augmentation_len: %x\n", 
+                        frame.augmentation_len);
 	}
 
-/* Get the info related with 'P' */
-frame.encoding=*ptr++;
-frame.personality_routine=*(unsigned char **)ptr;
-ptr+=sizeof(unsigned char *);
+        /* Get the info related with 'P' */
+        frame.encoding =* ptr++;
+        frame.personality_routine = *(unsigned char **)ptr;
+        ptr += sizeof(unsigned char *);
 
-if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
-	fprintf(stderr,"encoding: %x\n", frame.encoding);
-	fprintf(stderr,"per routine: %x\n", frame.personality_routine);
+        if (elfchk_debug & DEBUG_DWARF_CONTENTS) {
+                fprintf(stderr,"encoding: %x\n", frame.encoding);
+                fprintf(stderr,"per routine: %p\n", frame.personality_routine);
 	}
 
-/* Get the CFIs */
-
-if( elfchk_debug&DEBUG_DWARF_CONTENTS) {
-	fprintf(stderr,"%x bytes of CFI\n", ptr-(unsigned char *)frameimg);
+        /* Get the CFIs */
+        if (elfchk_debug&DEBUG_DWARF_CONTENTS) {
+                fprintf(stderr,"%x bytes of CFI\n", 
+                        ptr - (unsigned char *)frameimg);
 	}
 
-while( (ptr-(unsigned char *)frameimg) < frameimg->length+sizeof(int) ) {
-	ptr+=check_CFI(ptr,(frameimg->length+sizeof(int))-
-					(ptr-(unsigned char *)frameimg));
+        while ( (ptr - (unsigned char *)frameimg) < 
+                frameimg->length + sizeof(int) ) {
+                ptr += check_CFI(ptr, (frameimg->length+sizeof(int))-
+                                 (ptr-(unsigned char *)frameimg) );
 	}
 
-length-=frame.length;
-numused=frame.length;
+        length -= frame.length;
+        numused = frame.length;
 
-/* Align to 4 byte boundary */
-ptr=(unsigned char *)((long)(ptr+3)&~3);
+        /* Align to 4 byte boundary */
+        ptr = (unsigned char *)((long)(ptr+3)&~3);
 
-while(length>0) {
-	used-=check_FDE(ptr,length);
-	length-=used;
-	numused+=used;
+        while (length>0) {
+                used = check_FDE(ptr,length);
+                length -= used;
+                numused += used;
 	}
 
-return numused;
+        return numused;
 }
 
 void *
-read_FDE_encoded(unsigned char *ptr, unsigned char encoding,int *numused)
+read_FDE_encoded(unsigned char *ptr, unsigned char encoding, int *numused)
 {
-void *tmp;
+        void *tmp;
 
-if( encoding == DW_EH_PE_omit ) {
-	*numused=0;
-	return 0;
+        if (encoding == DW_EH_PE_omit) 
+        {
+                *numused = 0;
+                return 0;
 	}
 
-switch( encoding&0x0f ) {
+        switch (encoding & 0x0f) {
 	case DW_EH_PE_uleb128:
-		tmp=(void *)read_leb128(ptr,numused,0);
+		tmp = (void *) read_leb128(ptr,numused,0);
 		return tmp;
 	case DW_EH_PE_udata2:
-		tmp=(void *)(*(unsigned short *)ptr);
-		*numused=2;
+		tmp = (void *)(long)(*(unsigned short *)ptr);
+		*numused = 2;
 		return tmp;
 	case DW_EH_PE_udata4:
-		tmp=(void *)(*(unsigned int *)ptr);
-		*numused=4;
+		tmp = (void *)(*(unsigned int *)ptr);
+		*numused = 4;
 		return tmp;
 	case DW_EH_PE_udata8:
-		tmp=(void *)(*(unsigned long long *)ptr);
-		*numused=8;
+		tmp = (void *)(long)(*(unsigned long long *)ptr);
+		*numused = 8;
 		return tmp;
 	case DW_EH_PE_sleb128:
-		tmp=(void *)read_leb128(ptr,numused,1);
+		tmp = (void *)read_leb128(ptr,numused,1);
 		return tmp;
 	case DW_EH_PE_sdata2:
-		tmp=(void *)(*(short *)ptr);
-		*numused=2;
+		tmp = (void *)(long)(*(short *)ptr);
+		*numused = 2;
 		return tmp;
 	case DW_EH_PE_sdata4:
-		tmp=(void *)(*(int *)ptr);
-		*numused=4;
+		tmp = (void *)(*(int *)ptr);
+		*numused = 4;
 		return tmp;
 	case DW_EH_PE_sdata8:
-		tmp=(void *)(*(long long *)ptr);
-		*numused=8;
+		tmp = (void *)(long)(*(long long *)ptr);
+		*numused = 8;
 		return tmp;
 	default:
-		fprintf(stderr,"read_FDE_encoded() unexpected encoding %x\n",encoding);
+		fprintf(stderr,"read_FDE_encoded() unexpected encoding %x\n",
+                        encoding);
+                return NULL;
 	}
 }
