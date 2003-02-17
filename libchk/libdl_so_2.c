dc844b59480f6bfde5cb269dfce8a687172ac91a
cyeoh <>
2003-02-17 06:40:42 -0400

    resync against db. Adds S390X specific library versions

--- libchk/libdl_so_2.c
+++ libchk/libdl_so_2.c
@@ -1,8 +1,108 @@
 #include "elfchk.h"
 struct versym libdl_so_2[] = {
+#if __i386__
 	{"dladdr","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"dladdr","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"dladdr","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"dladdr","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"dladdr","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"dladdr","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"dladdr","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"dlclose","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"dlclose","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"dlclose","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"dlclose","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"dlclose","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"dlclose","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"dlclose","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"dlerror","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"dlerror","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"dlerror","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"dlerror","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"dlerror","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"dlerror","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"dlerror","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"dlopen","GLIBC_2.1"},
+#endif
+#if __ia64__
+	{"dlopen","GLIBC_2.1"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"dlopen","GLIBC_2.1"},
+#endif
+#if __powerpc64__
+	{"dlopen","GLIBC_2.1"},
+#endif
+#if __s390__ && !__s390x__
+	{"dlopen","GLIBC_2.1"},
+#endif
+#if __x86_64__
+	{"dlopen","GLIBC_2.1"},
+#endif
+#if __s390x__
+	{"dlopen","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"dlsym","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"dlsym","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"dlsym","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"dlsym","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"dlsym","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"dlsym","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"dlsym","GLIBC_2.2"},
+#endif
 	{0,0}};
--- libchk/libutil_so_1.c
+++ libchk/libutil_so_1.c
@@ -1,9 +1,129 @@
 #include "elfchk.h"
 struct versym libutil_so_1[] = {
+#if __i386__
 	{"forkpty","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"forkpty","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"forkpty","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"forkpty","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"forkpty","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"forkpty","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"forkpty","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"login","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"login","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"login","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"login","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"login","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"login","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"login","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"login_tty","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"login_tty","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"login_tty","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"login_tty","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"login_tty","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"login_tty","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"login_tty","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"logout","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"logout","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"logout","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"logout","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"logout","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"logout","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"logout","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"logwtmp","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"logwtmp","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"logwtmp","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"logwtmp","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"logwtmp","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"logwtmp","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"logwtmp","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"openpty","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"openpty","GLIBC_2.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"openpty","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"openpty","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"openpty","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"openpty","GLIBC_2.0"},
+#endif
+#if __s390x__
+	{"openpty","GLIBC_2.2"},
+#endif
 	{0,0}};
