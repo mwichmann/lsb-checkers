aef5a43572f8b150e164f7851dae0b20d18dec4b
anderson <>
2003-06-04 18:37:27 -0400

    Pick up x86_64 additionas and pthread additions

--- libchk/libpthread_so_0.c
+++ libchk/libpthread_so_0.c
@@ -2,6 +2,24 @@
 #include "elfchk.h"
 struct versym libpthread_so_0[] = {
 #if __i386__
+	{"pthread_atfork","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"pthread_atfork","GLIBC_2.2"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"pthread_atfork","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"pthread_atfork","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"pthread_atfork","GLIBC_2.2.5"},
+#endif
+#if __s390x__
+	{"pthread_atfork","GLIBC_2.2"},
+#endif
+#if __i386__
 	{"pthread_attr_destroy","GLIBC_2.0"},
 #endif
 #if __ia64__
@@ -399,6 +417,48 @@ struct versym libpthread_so_0[] = {
 #endif
 #if __s390x__
 	{"pthread_cancel","GLIBC_2.2"},
+#endif
+#if __i386__
+	{"pthread_cleanup_pop","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"pthread_cleanup_pop","GLIBC_2.2"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"pthread_cleanup_pop","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"pthread_cleanup_pop","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"pthread_cleanup_pop","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"pthread_cleanup_pop","GLIBC_2.2.5"},
+#endif
+#if __s390x__
+	{"pthread_cleanup_pop","GLIBC_2.2"},
+#endif
+#if __i386__
+	{"pthread_cleanup_push","GLIBC_2.0"},
+#endif
+#if __ia64__
+	{"pthread_cleanup_push","GLIBC_2.2"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"pthread_cleanup_push","GLIBC_2.0"},
+#endif
+#if __powerpc64__
+	{"pthread_cleanup_push","GLIBC_2.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"pthread_cleanup_push","GLIBC_2.0"},
+#endif
+#if __x86_64__
+	{"pthread_cleanup_push","GLIBC_2.2.5"},
+#endif
+#if __s390x__
+	{"pthread_cleanup_push","GLIBC_2.2"},
 #endif
 #if __i386__
 	{"pthread_cond_broadcast","GLIBC_2.0"},
