c7313d8842dd69895c5740ca453476ccd54ad718
anderson <>
2003-09-03 01:57:29 -0400

    Fill in some values for PPC32

--- devchk/ts/devchk/term_h.c
+++ devchk/ts/devchk/term_h.c
@@ -26,7 +26,9 @@ int pcnt=0;
 Msg("Checking data structures in term.h\n");
 #endif
 
-#if __i386__
+#if __powerpc__ && !__powerpc64__
+CheckTypeSize(TERMINAL,0, 10846, 6)
+#elif __i386__
 CheckTypeSize(TERMINAL,0, 10846, 2)
 #else
 Msg("REPLACE INTO ArchType VALUES (%d,%d,%d);\n",architecture,10846,0);
