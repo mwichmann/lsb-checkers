a0cf80cf0e721ed9ec60371d7ab9602f83d1486f
anderson <anderson>
2003-09-03 20:31:04 -0400

    More updated values for s390 and s390x and IA64

--- devchk/ts/devchk/netinet_in_h.c
+++ devchk/ts/devchk/netinet_in_h.c
@@ -265,15 +265,18 @@ CheckOffset(struct ip_mreq,imr_multiaddr,0,2,33768)
 CheckMemberSize(struct ip_mreq,imr_interface,4,2,33769)
 CheckOffset(struct ip_mreq,imr_interface,4,2,33769)
 #elif __s390__ && !__s390x__
-CheckTypeSize(struct ip_mreq,0, 10146, 10)
-Msg("Missing member data for ip_mreq on S390\n");
-CheckOffset(struct ip_mreq,imr_multiaddr,0,10,33768)
-CheckOffset(struct ip_mreq,imr_interface,0,10,33769)
+CheckTypeSize(struct ip_mreq,8, 10146, 10)
+CheckMemberSize(struct ip_mreq,imr_interface,4,10,33769)
+CheckOffset(struct ip_mreq,imr_interface,4,10,33769)
 #elif __s390x__
-CheckTypeSize(struct ip_mreq,0, 10146, 12)
-Msg("Missing member data for ip_mreq on S390X\n");
-CheckOffset(struct ip_mreq,imr_multiaddr,0,12,33768)
-CheckOffset(struct ip_mreq,imr_interface,0,12,33769)
+CheckTypeSize(struct ip_mreq,8, 10146, 12)
+CheckMemberSize(struct ip_mreq,imr_interface,4,12,33769)
+CheckOffset(struct ip_mreq,imr_interface,4,12,33769)
+#elif __ia64__
+CheckTypeSize(struct ip_mreq,0, 10146, 3)
+Msg("Missing member data for ip_mreq on IA64\n");
+CheckOffset(struct ip_mreq,imr_multiaddr,0,3,33768)
+CheckOffset(struct ip_mreq,imr_interface,0,3,33769)
 #else
 Msg("REPLACE INTO ArchType VALUES (%d,%d,%d);\n",architecture,10146,0);
 Msg("Find size of ip_mreq (10146)\n");
