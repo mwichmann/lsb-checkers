2a51e61ffde8a641111da5362d6e6af648e5c883
anderson <anderson>
2003-10-23 20:30:34 -0400

    Make the _Unwind function be per arch

--- libchk/libgcc_s_so_1.c
+++ libchk/libgcc_s_so_1.c
@@ -2,18 +2,384 @@
 #include "elfchk.h"
 struct versym libgcc_s_so_1[] = {
 	{"_Unwind_DeleteException","GCC_3.0"},
+#if __i386__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_DeleteException","GCC_3.0"},
+#endif
+	{"_Unwind_Find_FDE",""},
+#if __i386__
 	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_Find_FDE","GCC_3.0"},
+#endif
+	{"_Unwind_ForcedUnwind",""},
+#if __i386__
 	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_ForcedUnwind","GCC_3.0"},
+#endif
+	{"_Unwind_GetDataRelBase",""},
+	{"_Unwind_GetDataRelBase",""},
+	{"_Unwind_GetDataRelBase",""},
+	{"_Unwind_GetDataRelBase",""},
+	{"_Unwind_GetDataRelBase",""},
+	{"_Unwind_GetDataRelBase",""},
+	{"_Unwind_GetDataRelBase",""},
+	{"_Unwind_GetDataRelBase",""},
+#if __i386__
 	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
+	{"_Unwind_GetGR",""},
+#if __i386__
 	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_GetGR","GCC_3.0"},
+#endif
+	{"_Unwind_GetIP",""},
+#if __i386__
 	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_GetIP","GCC_3.0"},
+#endif
+	{"_Unwind_GetLanguageSpecificData",""},
+#if __i386__
 	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
+#endif
+	{"_Unwind_GetRegionStart",""},
+#if __i386__
 	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_GetRegionStart","GCC_3.0"},
+#endif
+	{"_Unwind_GetTextRelBase",""},
+#if __i386__
 	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_GetTextRelBase","GCC_3.0"},
+#endif
+	{"_Unwind_RaiseException",""},
+#if __i386__
 	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_RaiseException","GCC_3.0"},
+#endif
 	{"_Unwind_Resume","GCC_3.0"},
+	{"_Unwind_Resume","GCC_3.0"},
+	{"_Unwind_Resume","GCC_3.0"},
+	{"_Unwind_Resume","GCC_3.0"},
+	{"_Unwind_Resume","GCC_3.0"},
+	{"_Unwind_Resume",""},
+#if __i386__
+	{"_Unwind_Resume","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_Resume","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_Resume","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_Resume","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_Resume","GCC_3.0"},
+#endif
+	{"_Unwind_SetGR",""},
+#if __i386__
 	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_SetGR","GCC_3.0"},
+#endif
+	{"_Unwind_SetIP",""},
+#if __i386__
 	{"_Unwind_SetIP","GCC_3.0"},
+#endif
+#if __ia64__
+	{"_Unwind_SetIP","GCC_3.0"},
+#endif
+#if __arm__
+	{"_Unwind_SetIP","GCC_3.0"},
+#endif
+#if __mc68000__
+	{"_Unwind_SetIP","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_SetIP","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_SetIP","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_SetIP","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_SetIP","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_SetIP","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_SetIP","GCC_3.0"},
+#endif
 	{0,0}};
 
 struct classinfo *libgcc_s_so_1_classinfo[] = {
