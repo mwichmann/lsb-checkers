c8eefec74663ef523245eaa4e4ba1c55d5eccaa9
anderson <>
2003-10-28 18:44:31 -0400

    remove the 'None' version of symbols

--- libchk/libgcc_s_so_1.c
+++ libchk/libgcc_s_so_1.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "elfchk.h"
 struct versym libgcc_s_so_1[] = {
-	{"_Unwind_DeleteException","GCC_3.0"},
 #if __i386__
 	{"_Unwind_DeleteException","GCC_3.0"},
 #endif
@@ -32,7 +31,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_DeleteException","GCC_3.0"},
 #endif
-	{"_Unwind_Find_FDE",""},
 #if __i386__
 	{"_Unwind_Find_FDE","GCC_3.0"},
 #endif
@@ -63,7 +61,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_Find_FDE","GCC_3.0"},
 #endif
-	{"_Unwind_ForcedUnwind",""},
 #if __i386__
 	{"_Unwind_ForcedUnwind","GCC_3.0"},
 #endif
@@ -94,14 +91,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_ForcedUnwind","GCC_3.0"},
 #endif
-	{"_Unwind_GetDataRelBase",""},
-	{"_Unwind_GetDataRelBase",""},
-	{"_Unwind_GetDataRelBase",""},
-	{"_Unwind_GetDataRelBase",""},
-	{"_Unwind_GetDataRelBase",""},
-	{"_Unwind_GetDataRelBase",""},
-	{"_Unwind_GetDataRelBase",""},
-	{"_Unwind_GetDataRelBase",""},
 #if __i386__
 	{"_Unwind_GetDataRelBase","GCC_3.0"},
 #endif
@@ -111,7 +100,27 @@ struct versym libgcc_s_so_1[] = {
 #if __arm__
 	{"_Unwind_GetDataRelBase","GCC_3.0"},
 #endif
-	{"_Unwind_GetGR",""},
+#if __mc68000__
+	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
+#if __alpha__
+	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
+#if __powerpc64__
+	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
+#if __s390__ && !__s390x__
+	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
+#if __x86_64__
+	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
+#if __s390x__
+	{"_Unwind_GetDataRelBase","GCC_3.0"},
+#endif
 #if __i386__
 	{"_Unwind_GetGR","GCC_3.0"},
 #endif
@@ -142,7 +151,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_GetGR","GCC_3.0"},
 #endif
-	{"_Unwind_GetIP",""},
 #if __i386__
 	{"_Unwind_GetIP","GCC_3.0"},
 #endif
@@ -173,7 +181,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_GetIP","GCC_3.0"},
 #endif
-	{"_Unwind_GetLanguageSpecificData",""},
 #if __i386__
 	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
 #endif
@@ -204,7 +211,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_GetLanguageSpecificData","GCC_3.0"},
 #endif
-	{"_Unwind_GetRegionStart",""},
 #if __i386__
 	{"_Unwind_GetRegionStart","GCC_3.0"},
 #endif
@@ -235,7 +241,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_GetRegionStart","GCC_3.0"},
 #endif
-	{"_Unwind_GetTextRelBase",""},
 #if __i386__
 	{"_Unwind_GetTextRelBase","GCC_3.0"},
 #endif
@@ -266,7 +271,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_GetTextRelBase","GCC_3.0"},
 #endif
-	{"_Unwind_RaiseException",""},
 #if __i386__
 	{"_Unwind_RaiseException","GCC_3.0"},
 #endif
@@ -302,7 +306,6 @@ struct versym libgcc_s_so_1[] = {
 	{"_Unwind_Resume","GCC_3.0"},
 	{"_Unwind_Resume","GCC_3.0"},
 	{"_Unwind_Resume","GCC_3.0"},
-	{"_Unwind_Resume",""},
 #if __i386__
 	{"_Unwind_Resume","GCC_3.0"},
 #endif
@@ -318,7 +321,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_Resume","GCC_3.0"},
 #endif
-	{"_Unwind_SetGR",""},
 #if __i386__
 	{"_Unwind_SetGR","GCC_3.0"},
 #endif
@@ -349,7 +351,6 @@ struct versym libgcc_s_so_1[] = {
 #if __s390x__
 	{"_Unwind_SetGR","GCC_3.0"},
 #endif
-	{"_Unwind_SetIP",""},
 #if __i386__
 	{"_Unwind_SetIP","GCC_3.0"},
 #endif
