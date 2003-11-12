335cbe7245d9d675d04ec73801d5ec9ece92de9e
mats <>
2003-11-12 01:46:08 -0400

    Revision pass to make a "Journal" a class so it can hold state and simplify a lot of the passing around of variables.

--- cmdchk/cmdchk.py
+++ cmdchk/cmdchk.py
@@ -7,14 +7,18 @@ Usage: cmdchk [prefix]
 Prefix, if supplied, is a prefix to prepend to all paths
 '''
 
-# (C) Copyright 2002 The Free Standards Group, Inc.
+# (C) Copyright 2002, 2003 The Free Standards Group, Inc.
 #
 # Python version
-# 2002/09/20 Mats Wichmann, Intel
+# Author: Mats Wichmann, Intel Corporation
 #
-# This is $Revision: 1.1 $
+# This is $Revision: 1.2 $
 #
 # $Log: cmdchk.py,v $
+# Revision 1.2  2003/11/12 00:46:08  mats
+# Revision pass to make a "Journal" a class so it can hold state and
+# simplify a lot of the passing around of variables.
+#
 # Revision 1.1  2002/09/20 15:52:48  mwichmann
 # Added Python version of cmdchk.  This is an experiment to further test out
 # the Python tetj, it's not proposed to add to the lsbcmdchk pkg at this time
@@ -36,43 +40,35 @@ binpaths = [
 prefix = ""
 
 
-def check_cmd(cmdname, cmdpath, journal):
-    tetj.testcase_start(journal, tetj.activity_count, cmdname, "")
-    tetj.tp_count = 0
-
-    tetj.tp_count += 1
+def check_cmd(journal, cmdname, cmdpath):
+    journal.testcase_start(cmdname)
     tmp_string = "Looking for command %s" % cmdname
-    tetj.purpose_start(journal, tetj.activity_count, tetj.tp_count, tmp_string)
+    journal.purpose_start(tmp_string)
 
     if cmdname[0] != '/':
         # Check each of the directories in binpaths for the command
         for i in binpaths:
             filename = prefix + i + cmdname
             if os.access(filename,R_OK|X_OK):
-                tetj.result(journal, tetj.activity_count, tetj.tp_count,
-                            tetj.TETJ_PASS)
+                journal.result(tetj.TETJ_PASS)
                 break
             if os.path.exists(filename):
                 # access() failed but it exists: issue a FIP
                 # (probably need to be running as root)
                 sys.stderr.write("Couldn't access %s\n" % cmdname)
                 tmp_string = "File found cannot be accessed: " + filename
-                tetj.testcase_info(journal, tetj.activity_count, tetj.tp_count,
-                                   0, 1, 1, tmp_string)
-                tetj.result(journal, tetj.activity_count, tetj.tp_count,
-                            tetj.TETJ_FIP)
+                journal.testcase_info(0, 1, 1, tmp_string)
+                journal.result(tetj.TETJ_FIP)
                 break
         else:
             # not found at all
             sys.stderr.write("Couldn't find %s\n" % cmdname)
-            tetj.result(journal, tetj.activity_count, tetj.tp_count, 
-                        tetj.TETJ_FAIL)
+            journal.result(tetj.TETJ_FAIL)
     # else:     # duplicate most of the above logic to seach full paths
                 # (not used currently). no need to loop through binpaths
 
-    tetj.purpose_end(journal, tetj.activity_count, tetj.tp_count)
-    tetj.testcase_end(journal, tetj.activity_count, cmdname, "")
-    tetj.activity_count += 1
+    journal.purpose_end()
+    journal.testcase_end()
 
 def parse_cmds(cmdfile):
     cmds = []
@@ -85,17 +81,17 @@ def parse_cmds(cmdfile):
 
 if len(sys.argv) > 1:
         prefix = sys.argv[1]
-journal = tetj.start_journal("journal.lsbcmdchk", "lsbcmdchk")
-if not journal:
+journal = tetj.Journal("journal.lsbcmdchk", "lsbcmdchk")
+if not journal.journal:
     sys.stderr.write("Could not open journal file")
     sys.exit(1)
 
 tmp_string = "VSX_NAME=lsbcmdchk experimental"
-tetj.add_config(journal, tmp_string)
+journal.add_config(tmp_string)
 tmp_string = "search prefix is [%s]" % prefix
-tetj.add_config(journal, tmp_string)
+journal.add_config(tmp_string)
 items = parse_cmds("cmdlist")
 for (command, cmdpath) in items:
-    check_cmd(command, cmdpath, journal)
-tetj.close_journal(journal)
+    check_cmd(journal, command, cmdpath)
+journal.close()
 sys.exit(0)
