e8d3f503aaa472afe8e7102439a54ef62b6ef2bb
anderson <>
2003-12-16 22:40:08 -0400

    Fix size & all missing archs

--- devchk/ts/devchk/term_h.c
+++ devchk/ts/devchk/term_h.c
@@ -26,26 +26,47 @@ int pcnt=0;
 Msg("Checking data structures in term.h\n");
 #endif
 
-#if __i386__
+#if __powerpc64__
+CheckTypeSize(struct term,200, 10850, 9)
+Msg("Missing member data for term on PPC64\n");
+#elif __i386__
 CheckTypeSize(struct term,168, 10850, 2)
 Msg("Missing member data for term on IA32\n");
+#elif __x86_64__
+CheckTypeSize(struct term,200, 10850, 11)
+Msg("Missing member data for term on x86-64\n");
+#elif __s390__ && !__s390x__
+CheckTypeSize(struct term,168, 10850, 10)
+Msg("Missing member data for term on S390\n");
+#elif __arm__
+CheckTypeSize(struct term,168, 10850, 4)
+Msg("Missing member data for term on ARM\n");
+#elif __ia64__
+CheckTypeSize(struct term,200, 10850, 3)
+Msg("Missing member data for term on IA64\n");
+#elif __s390x__
+CheckTypeSize(struct term,200, 10850, 12)
+Msg("Missing member data for term on S390X\n");
+#elif __powerpc__ && !__powerpc64__
+CheckTypeSize(struct term,168, 10850, 6)
+Msg("Missing member data for term on PPC32\n");
 #else
 Msg("REPLACE INTO ArchType VALUES (%d,%d,%d);\n",architecture,10850,0);
 Msg("Find size of term (10850)\n");
 #endif
 
 #if __powerpc__ && !__powerpc64__
-CheckTypeSize(TERMINAL,0, 10846, 6)
+CheckTypeSize(TERMINAL,168, 10846, 6)
 #elif __i386__
 CheckTypeSize(TERMINAL,168, 10846, 2)
 #elif __s390__ && !__s390x__
-CheckTypeSize(TERMINAL,0, 10846, 10)
+CheckTypeSize(TERMINAL,168, 10846, 10)
 #elif __s390x__
-CheckTypeSize(TERMINAL,0, 10846, 12)
+CheckTypeSize(TERMINAL,200, 10846, 12)
 #elif __ia64__
 CheckTypeSize(TERMINAL,200, 10846, 3)
 #elif __powerpc64__
-CheckTypeSize(TERMINAL,0, 10846, 9)
+CheckTypeSize(TERMINAL,200, 10846, 9)
 #elif __x86_64__
 CheckTypeSize(TERMINAL,200, 10846, 11)
 #else
