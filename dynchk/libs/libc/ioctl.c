2d7307c483d9ac13fbe05bbcaefec092adea115a
anderson <>
2004-01-17 16:12:38 -0400

    Add ioctl(), which should be very interesting

---
+++ dynchk/libs/libc/ioctl.c
@@ -0,0 +1,27 @@
+#include "type_tests.h"
+#include <dlfcn.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static int(*funcptr)(int, int, const void *) = 0;
+
+int ioctl(int fd, int flags, int request, const void *arg)
+{
+	if(!funcptr)
+		funcptr = dlsym(RTLD_NEXT, "ioctl");
+
+	validate_filedescriptor(fd, "ioctl");
+	validate_ioctlreq(request, "ioctl");
+	validate_RWaddress(arg, "ioctl");
+	
+	return funcptr(fd, request, arg);
+}
+
+int lsb_ioctl(int fd, int flags, int request, const void *arg)
+{
+	if(!funcptr)
+		funcptr = dlsym(RTLD_NEXT, "ioctl");
+
+	return funcptr(fd, request, arg);
+}
+
