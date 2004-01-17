756d3bb7fddb56607c454f51d2afc0f16c47a596
anderson <anderson>
2004-01-17 16:12:58 -0400

    Fix the use of var-args

--- dynchk/libs/libc/printf.c
+++ dynchk/libs/libc/printf.c
@@ -1,23 +1,29 @@
 #include "type_tests.h"
 #include <dlfcn.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
  
+static int (*funcptr)(const char *, ...) = 0;
+
 int printf(const char *format, ...)
 {	
 	va_list args;
-	static ssize_t(*funcptr)(const char *, ...) = 0;
-panic("printf called!!\n");
+	va_start(args, format);
+
+lsb_printf("printf called!! funcptr=%p\n",funcptr);
 	if(!funcptr)
 		funcptr = dlsym(RTLD_NEXT, "vprintf");
 	validate_RWaddress(format, "printf");
+lsb_printf("printf calling %p\n",funcptr);
 	return funcptr(format, args);
 }
 
 int lsb_printf(const char *format, ...)
 {	
 	va_list args;
-	static ssize_t(*funcptr)(const char *, ...) = 0;
+	va_start(args, format);
+	//static int (*funcptr)(const char *, ...) = 0;
 	if(!funcptr)
 		funcptr = dlsym(RTLD_NEXT, "vprintf");
 	return funcptr(format, args);
