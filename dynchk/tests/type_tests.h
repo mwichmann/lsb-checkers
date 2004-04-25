3fcb85081c0e3ea9ca4c260dd394518c96c9419f
elderm <elderm>
2004-04-25 03:36:40 -0400

    added validate_NULL_TYPETYPE macro

--- dynchk/tests/type_tests.h
+++ dynchk/tests/type_tests.h
@@ -3,9 +3,15 @@
 
 // This file gets included by every wrapper function file.
 
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE //for RTLD_NEXT in dlfcn
+#endif
+#define validate_NULL_TYPETYPE(x) //validate_NULL_TYPETYPE is a placeholder.
+
 #include <dlfcn.h>  //for dlsym() in wrapper functions
 #include <sys/types.h>
+
+#include "struct_tests_h.h"
 
 extern void validate_pathname(const char *arg, const char *name);
 extern void validate_filemode(const mode_t arg, const char *name);
