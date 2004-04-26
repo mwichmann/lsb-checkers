cd6a889b3332b3b240d034b43dec43bbc8ce7d60
anderson <>
2004-04-26 04:30:01 -0400

    Add the scanf family

---
+++ dynchk/libs/libc/fscanf.c
@@ -0,0 +1,25 @@
+#include "type_tests.h"
+#include <dlfcn.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+ 
+static int (*funcptr)(FILE *f, const char *, ...) = 0;
+
+int fscanf(FILE *f, const char *format, ...)
+{	
+	va_list args;
+	va_start(args, format);
+	if(!funcptr)
+		funcptr = dlsym(RTLD_NEXT, "vfscanf");
+	return funcptr(f, format, args);
+}
+
+int lsb_fscanf(FILE *f, const char *format, ...)
+{	
+	va_list args;
+	va_start(args, format);
+	if(!funcptr)
+		funcptr = dlsym(RTLD_NEXT, "vfscanf");
+	return funcptr(f, format, args);
+}
---
+++ dynchk/libs/libc/sscanf.c
@@ -0,0 +1,28 @@
+#include "type_tests.h"
+#include <dlfcn.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+ 
+static int (*funcptr)(const char *str, const char *, ...) = 0;
+
+int sscanf(const char *str, const char *format, ...)
+{	
+	va_list args;
+	va_start(args, format);
+
+	if(!funcptr)
+		funcptr = dlsym(RTLD_NEXT, "vsscanf");
+	validate_Rdaddress(str, "sscanf-1");
+	validate_Rdaddress(format, "sscanf-2");
+	return funcptr(str, format, args);
+}
+
+int lsb_sscanf(const char *str, const char *format, ...)
+{	
+	va_list args;
+	va_start(args, format);
+	if(!funcptr)
+		funcptr = dlsym(RTLD_NEXT, "vsscanf");
+	return funcptr(str, format, args);
+}
