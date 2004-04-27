20842e67c31b13505f226431d8bd8be372e14292
anderson <anderson>
2004-04-27 01:01:31 -0400

    Accept FIONREAD also

--- dynchk/tests/ioctlreq.c
+++ dynchk/tests/ioctlreq.c
@@ -8,10 +8,14 @@
 void validate_ioctlreq(const int req, const char *name)
 {
 	switch(req) {
+		/* Socket related icotl()s */
 	case SIOCGIFCONF:
 	case SIOCGIFFLAGS:
 	case SIOCGIFADDR:
 	case SIOCGIFNETMASK:
+		break;
+		/* file related icotl()s */
+	case FIONREAD:
 		break;
 	default:
 		lsb_fprintf(stderr,"Illegal ioctl() request 0x%x\n", req );
