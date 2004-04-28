b46cfa6d8ff033cd40451a75fef60426bbdb844e
anderson <>
2004-04-27 20:29:17 -0400

    More function used by the tests

--- dynchk/libs/lsb_funcs.h
+++ dynchk/libs/lsb_funcs.h
@@ -2,4 +2,7 @@
 #include <unistd.h>
 
 int lsb_fprintf(FILE * a0, const char * a1, ... );
+int lsb_fscanf(FILE * a0, const char * a1, ... );
 long lsb_sysconf(int a0);
+FILE *lsb_fopen(const char *,const char *);
+void * lsb_realloc(void *, size_t);
