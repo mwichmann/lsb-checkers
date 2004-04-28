0c78bf7f483c171fa4036d3683e0e8fff483c60a
anderson <anderson>
2004-04-27 20:29:26 -0400

    Add the gen.mk files

---
+++ dynchk/libs/libgcc_s/gen.mk
@@ -0,0 +1,14 @@
+GEN_FUNCS = \
+_Unwind_DeleteException.o \
+_Unwind_Find_FDE.o \
+_Unwind_ForcedUnwind.o \
+_Unwind_GetDataRelBase.o \
+_Unwind_GetGR.o \
+_Unwind_GetIP.o \
+_Unwind_GetLanguageSpecificData.o \
+_Unwind_GetRegionStart.o \
+_Unwind_GetTextRelBase.o \
+_Unwind_RaiseException.o \
+_Unwind_Resume.o \
+_Unwind_SetGR.o \
+_Unwind_SetIP.o 
---
+++ dynchk/libs/libpam/gen.mk
@@ -0,0 +1,14 @@
+GEN_FUNCS = \
+pam_set_item.o \
+pam_get_item.o \
+pam_strerror.o \
+pam_getenvlist.o \
+pam_fail_delay.o \
+pam_start.o \
+pam_end.o \
+pam_authenticate.o \
+pam_setcred.o \
+pam_acct_mgmt.o \
+pam_open_session.o \
+pam_close_session.o \
+pam_chauthtok.o 
