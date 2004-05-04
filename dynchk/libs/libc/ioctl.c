e14c13e2f5e53603ce6b933f08e40fddcfbfdd39
anderson <anderson>
2004-05-04 19:48:07 -0400

    Hide the ioctl test from tc[gs]etattr()

--- dynchk/libs/libc/ioctl.c
+++ dynchk/libs/libc/ioctl.c
@@ -5,6 +5,24 @@
 
 static int(*funcptr)(int, unsigned long, ...) = 0;
 
+/*
+ * As long as an ioctl is hidden behind an LSB interface, it's OK for it
+ * to be used. It's just an implementation detail. We need a way for an LSB
+ * interface to let us know that it will be using ioctl() internally, and that
+ * we shouldn't complain about it.
+ */
+static ioctlok=0;
+
+void __lsb_permit_ioctl()
+{
+ioctlok=1;
+}
+
+void __lsb_forbid_ioctl()
+{
+ioctlok=0;
+}
+
 int ioctl(int fd, unsigned long request, ...)
 {
 	va_list arg;
@@ -13,9 +31,11 @@ int ioctl(int fd, unsigned long request, ...)
 	if(!funcptr)
 		funcptr = dlsym(RTLD_NEXT, "ioctl");
 
-	validate_filedescriptor(fd, "ioctl");
-	validate_ioctlreq(request, "ioctl");
-	validate_RWaddress(arg, "ioctl");
+	if( ioctlok) {
+		validate_filedescriptor(fd, "ioctl");
+		validate_ioctlreq(request, "ioctl");
+		validate_RWaddress(arg, "ioctl");
+		}
 	
 	return funcptr(fd, request, arg);
 }
