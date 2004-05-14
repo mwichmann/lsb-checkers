35af0c8e0d2bd90ad578481320e6482be13e22d8
elderm <>
2004-05-14 19:29:10 -0400

    add stubs so we can compile more nicely.

---
+++ dynchk/tests/IPv4Address.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <netinet/in.h>
+
+void validate_IPv4Address(struct in_addr *input, const char *name)
+{
+	//stub.
+	return 0;
+}
---
+++ dynchk/tests/socketport.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <netinet/in.h>
+
+void validate_socketport(struct sa_port_t input, const char *name)
+{
+	//stub.
+	return 0;
+}
