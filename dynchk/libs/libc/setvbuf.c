b73d077fb9d4535e5d626121c43f61dff930b1f9
anderson <anderson>
2004-05-21 12:26:59 -0400

    Handle optional NULL param

--- dynchk/libs/libc/setvbuf.c
+++ dynchk/libs/libc/setvbuf.c
@@ -13,8 +13,10 @@ int setvbuf (FILE * arg0 , char * arg1 , int arg2 , size_t arg3 )
 		funcptr = dlsym(RTLD_NEXT, "setvbuf");
 	validate_Rdaddress( arg0, "setvbuf - arg0");
 	validate_NULL_TYPETYPE(  arg0, "setvbuf - arg0");
+	if( arg1 ) {
 	validate_Rdaddress( arg1, "setvbuf - arg1");
 	validate_NULL_TYPETYPE(  arg1, "setvbuf - arg1");
+	}
 	validate_NULL_TYPETYPE(  arg2, "setvbuf - arg2");
 	validate_NULL_TYPETYPE(  arg3, "setvbuf - arg3");
 	return funcptr(arg0, arg1, arg2, arg3);
