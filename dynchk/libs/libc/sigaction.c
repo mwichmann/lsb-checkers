81518fb9f5e6b659389f4a98c3085bc2935a22da
elderm <elderm>
2004-06-08 16:15:30 -0400

    NULL is ok on arg1 and arg2.

--- dynchk/libs/libc/sigaction.c
+++ dynchk/libs/libc/sigaction.c
@@ -19,9 +19,13 @@ int sigaction (int arg0 , const struct sigaction * arg1 , struct sigaction * arg
 		__lsb_check_params=0;
 	__lsb_output(5-__lsb_check_params, "sigaction()");
 		validate_NULL_TYPETYPE(  arg0, "sigaction - arg0");
+		if( arg1 ) {
 	validate_Rdaddress( arg1, "sigaction - arg1");
+		}
 		validate_NULL_TYPETYPE(  arg1, "sigaction - arg1");
+		if( arg2 ) {
 	validate_Rdaddress( arg2, "sigaction - arg2");
+		}
 		validate_NULL_TYPETYPE(  arg2, "sigaction - arg2");
 	}
 	ret_value = funcptr(arg0, arg1, arg2);
