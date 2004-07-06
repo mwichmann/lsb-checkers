1c2ead6d22c0c2006edd569dd92093b0e40a732c
anderson <anderson>
2004-07-05 22:40:24 -0400

    cache the result of sysconf(_SC_OPEN_MAX) to speed things up a lot

--- dynchk/tests/filedescriptor.c
+++ dynchk/tests/filedescriptor.c
@@ -2,20 +2,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+static int curmaxfd = 0;
+
 int validate_filedescriptor(const int fd, const char *name)
 {
-	if(fd >= sysconf(_SC_OPEN_MAX))
+	if(fd < 0)
 	{
-		fprintf(stderr, "lsbdynchk: %s: File descriptor %x is too high.\n",
-					name);
-		fprintf(stderr,"\t%x is the highest value this system allows.\n",
-					fd, sysconf(_SC_OPEN_MAX) );
+		fprintf(stderr,
+		   "lsbdynchk: %s: File descriptor %x is negative, and thus invalid.\n",
+		   name, fd);
 		return 1;
 	}
-	else if(fd<0)
+
+	if(fd > curmaxfd) {
+		fprintf(stderr,"old curmaxfd = %d\n", curmaxfd );
+		curmaxfd=sysconf(_SC_OPEN_MAX);
+		fprintf(stderr,"new curmaxfd = %d\n", curmaxfd );
+	}
+
+	if(fd >= curmaxfd )
 	{
-	fprintf(stderr, "lsbdynchk: %s: File descriptor %x is negative, and thus invalid.\n", name, fd);
-	return 1;
+		fprintf(stderr,
+			"lsbdynchk: %s: File descriptor %x is too high.\n",
+			name, fd );
+		fprintf(stderr,
+			"\t%x is the highest value this system allows.\n",
+			sysconf(_SC_OPEN_MAX) );
+		return 1;
 	}
 	return 0;
 }
