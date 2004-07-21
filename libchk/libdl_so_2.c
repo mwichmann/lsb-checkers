5ba86b35ecaaec42fddff19d39a81ef9c85bc21b
anderson <anderson>
2004-07-21 16:29:38 -0400

    Fill in the Module names correctly, and resync w/ recent changes

--- libchk/libdl_so_2.c
+++ libchk/libdl_so_2.c
@@ -2,109 +2,109 @@
 #include "elfchk.h"
 struct versym libdl_so_2[] = {
 #if __i386__
-	{"dladdr","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dladdr","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __ia64__
-	{"dladdr","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dladdr","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"dladdr","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dladdr","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc64__
-	{"dladdr","GLIBC_2.3",0,"GLIBC_2.3",4,1},
+	{"dladdr","GLIBC_2.3",0,"LSB-Core",4,1},
 #endif
 #if __s390__ && !__s390x__
-	{"dladdr","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dladdr","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __x86_64__
-	{"dladdr","GLIBC_2.2.5",0,"GLIBC_2.2.5",4,1},
+	{"dladdr","GLIBC_2.2.5",0,"LSB-Core",4,1},
 #endif
 #if __s390x__
-	{"dladdr","GLIBC_2.2",0,"GLIBC_2.2",4,1},
+	{"dladdr","GLIBC_2.2",0,"LSB-Core",4,1},
 #endif
 #if __i386__
-	{"dlclose","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dlclose","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __ia64__
-	{"dlclose","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dlclose","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"dlclose","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dlclose","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc64__
-	{"dlclose","GLIBC_2.3",0,"GLIBC_2.3",4,1},
+	{"dlclose","GLIBC_2.3",0,"LSB-Core",4,1},
 #endif
 #if __s390__ && !__s390x__
-	{"dlclose","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dlclose","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __x86_64__
-	{"dlclose","GLIBC_2.2.5",0,"GLIBC_2.2.5",4,1},
+	{"dlclose","GLIBC_2.2.5",0,"LSB-Core",4,1},
 #endif
 #if __s390x__
-	{"dlclose","GLIBC_2.2",0,"GLIBC_2.2",4,1},
+	{"dlclose","GLIBC_2.2",0,"LSB-Core",4,1},
 #endif
 #if __i386__
-	{"dlerror","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dlerror","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __ia64__
-	{"dlerror","GLIBC_2.0",0,"GLIBC_2.0",8,1},
+	{"dlerror","GLIBC_2.0",0,"LSB-Core",8,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"dlerror","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dlerror","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc64__
-	{"dlerror","GLIBC_2.3",0,"GLIBC_2.3",8,1},
+	{"dlerror","GLIBC_2.3",0,"LSB-Core",8,1},
 #endif
 #if __s390__ && !__s390x__
-	{"dlerror","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"dlerror","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __x86_64__
-	{"dlerror","GLIBC_2.2.5",0,"GLIBC_2.2.5",8,1},
+	{"dlerror","GLIBC_2.2.5",0,"LSB-Core",8,1},
 #endif
 #if __s390x__
-	{"dlerror","GLIBC_2.2",0,"GLIBC_2.2",8,1},
+	{"dlerror","GLIBC_2.2",0,"LSB-Core",8,1},
 #endif
 #if __i386__
-	{"dlopen","GLIBC_2.1",0,"GLIBC_2.1",0,1},
+	{"dlopen","GLIBC_2.1",0,"LSB-Core",0,1},
 #endif
 #if __ia64__
-	{"dlopen","GLIBC_2.1",0,"GLIBC_2.1",0,1},
+	{"dlopen","GLIBC_2.1",0,"LSB-Core",0,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"dlopen","GLIBC_2.1",0,"GLIBC_2.1",0,1},
+	{"dlopen","GLIBC_2.1",0,"LSB-Core",0,1},
 #endif
 #if __powerpc64__
-	{"dlopen","GLIBC_2.3",0,"GLIBC_2.3",0,1},
+	{"dlopen","GLIBC_2.3",0,"LSB-Core",0,1},
 #endif
 #if __s390__ && !__s390x__
-	{"dlopen","GLIBC_2.1",0,"GLIBC_2.1",0,1},
+	{"dlopen","GLIBC_2.1",0,"LSB-Core",0,1},
 #endif
 #if __x86_64__
-	{"dlopen","GLIBC_2.2.5",0,"GLIBC_2.2.5",0,1},
+	{"dlopen","GLIBC_2.2.5",0,"LSB-Core",0,1},
 #endif
 #if __s390x__
-	{"dlopen","GLIBC_2.2",0,"GLIBC_2.2",0,1},
+	{"dlopen","GLIBC_2.2",0,"LSB-Core",0,1},
 #endif
 #if __i386__
-	{"dlsym","GLIBC_2.0",0,"GLIBC_2.0",0,1},
+	{"dlsym","GLIBC_2.0",0,"LSB-Core",0,1},
 #endif
 #if __ia64__
-	{"dlsym","GLIBC_2.0",0,"GLIBC_2.0",0,1},
+	{"dlsym","GLIBC_2.0",0,"LSB-Core",0,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"dlsym","GLIBC_2.0",0,"GLIBC_2.0",0,1},
+	{"dlsym","GLIBC_2.0",0,"LSB-Core",0,1},
 #endif
 #if __powerpc64__
-	{"dlsym","GLIBC_2.3",0,"GLIBC_2.3",0,1},
+	{"dlsym","GLIBC_2.3",0,"LSB-Core",0,1},
 #endif
 #if __s390__ && !__s390x__
-	{"dlsym","GLIBC_2.0",0,"GLIBC_2.0",0,1},
+	{"dlsym","GLIBC_2.0",0,"LSB-Core",0,1},
 #endif
 #if __x86_64__
-	{"dlsym","GLIBC_2.2.5",0,"GLIBC_2.2.5",0,1},
+	{"dlsym","GLIBC_2.2.5",0,"LSB-Core",0,1},
 #endif
 #if __s390x__
-	{"dlsym","GLIBC_2.2",0,"GLIBC_2.2",0,1},
+	{"dlsym","GLIBC_2.2",0,"LSB-Core",0,1},
 #endif
 	{0,0}};
 
--- libchk/libutil_so_1.c
+++ libchk/libutil_so_1.c
@@ -2,130 +2,130 @@
 #include "elfchk.h"
 struct versym libutil_so_1[] = {
 #if __i386__
-	{"forkpty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"forkpty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __ia64__
-	{"forkpty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"forkpty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"forkpty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"forkpty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc64__
-	{"forkpty","GLIBC_2.3",0,"GLIBC_2.3",4,1},
+	{"forkpty","GLIBC_2.3",0,"LSB-Core",4,1},
 #endif
 #if __s390__ && !__s390x__
-	{"forkpty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"forkpty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __x86_64__
-	{"forkpty","GLIBC_2.2.5",0,"GLIBC_2.2.5",4,1},
+	{"forkpty","GLIBC_2.2.5",0,"LSB-Core",4,1},
 #endif
 #if __s390x__
-	{"forkpty","GLIBC_2.2",0,"GLIBC_2.2",4,1},
+	{"forkpty","GLIBC_2.2",0,"LSB-Core",4,1},
 #endif
 #if __i386__
-	{"login","GLIBC_2.0",0,"GLIBC_2.0",1,1},
+	{"login","GLIBC_2.0",0,"LSB-Core",1,1},
 #endif
 #if __ia64__
-	{"login","GLIBC_2.0",0,"GLIBC_2.0",1,1},
+	{"login","GLIBC_2.0",0,"LSB-Core",1,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"login","GLIBC_2.0",0,"GLIBC_2.0",1,1},
+	{"login","GLIBC_2.0",0,"LSB-Core",1,1},
 #endif
 #if __powerpc64__
-	{"login","GLIBC_2.3",0,"GLIBC_2.3",1,1},
+	{"login","GLIBC_2.3",0,"LSB-Core",1,1},
 #endif
 #if __s390__ && !__s390x__
-	{"login","GLIBC_2.0",0,"GLIBC_2.0",1,1},
+	{"login","GLIBC_2.0",0,"LSB-Core",1,1},
 #endif
 #if __x86_64__
-	{"login","GLIBC_2.2.5",0,"GLIBC_2.2.5",1,1},
+	{"login","GLIBC_2.2.5",0,"LSB-Core",1,1},
 #endif
 #if __s390x__
-	{"login","GLIBC_2.2",0,"GLIBC_2.2",1,1},
+	{"login","GLIBC_2.2",0,"LSB-Core",1,1},
 #endif
 #if __i386__
-	{"login_tty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"login_tty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __ia64__
-	{"login_tty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"login_tty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"login_tty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"login_tty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc64__
-	{"login_tty","GLIBC_2.3",0,"GLIBC_2.3",4,1},
+	{"login_tty","GLIBC_2.3",0,"LSB-Core",4,1},
 #endif
 #if __s390__ && !__s390x__
-	{"login_tty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"login_tty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __x86_64__
-	{"login_tty","GLIBC_2.2.5",0,"GLIBC_2.2.5",4,1},
+	{"login_tty","GLIBC_2.2.5",0,"LSB-Core",4,1},
 #endif
 #if __s390x__
-	{"login_tty","GLIBC_2.2",0,"GLIBC_2.2",4,1},
+	{"login_tty","GLIBC_2.2",0,"LSB-Core",4,1},
 #endif
 #if __i386__
-	{"logout","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"logout","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __ia64__
-	{"logout","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"logout","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"logout","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"logout","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc64__
-	{"logout","GLIBC_2.3",0,"GLIBC_2.3",4,1},
+	{"logout","GLIBC_2.3",0,"LSB-Core",4,1},
 #endif
 #if __s390__ && !__s390x__
-	{"logout","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"logout","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __x86_64__
-	{"logout","GLIBC_2.2.5",0,"GLIBC_2.2.5",4,1},
+	{"logout","GLIBC_2.2.5",0,"LSB-Core",4,1},
 #endif
 #if __s390x__
-	{"logout","GLIBC_2.2",0,"GLIBC_2.2",4,1},
+	{"logout","GLIBC_2.2",0,"LSB-Core",4,1},
 #endif
 #if __i386__
-	{"logwtmp","GLIBC_2.0",0,"GLIBC_2.0",1,1},
+	{"logwtmp","GLIBC_2.0",0,"LSB-Core",1,1},
 #endif
 #if __ia64__
-	{"logwtmp","GLIBC_2.0",0,"GLIBC_2.0",1,1},
+	{"logwtmp","GLIBC_2.0",0,"LSB-Core",1,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"logwtmp","GLIBC_2.0",0,"GLIBC_2.0",1,1},
+	{"logwtmp","GLIBC_2.0",0,"LSB-Core",1,1},
 #endif
 #if __powerpc64__
-	{"logwtmp","GLIBC_2.3",0,"GLIBC_2.3",1,1},
+	{"logwtmp","GLIBC_2.3",0,"LSB-Core",1,1},
 #endif
 #if __s390__ && !__s390x__
-	{"logwtmp","GLIBC_2.0",0,"GLIBC_2.0",1,1},
+	{"logwtmp","GLIBC_2.0",0,"LSB-Core",1,1},
 #endif
 #if __x86_64__
-	{"logwtmp","GLIBC_2.2.5",0,"GLIBC_2.2.5",1,1},
+	{"logwtmp","GLIBC_2.2.5",0,"LSB-Core",1,1},
 #endif
 #if __s390x__
-	{"logwtmp","GLIBC_2.2",0,"GLIBC_2.2",1,1},
+	{"logwtmp","GLIBC_2.2",0,"LSB-Core",1,1},
 #endif
 #if __i386__
-	{"openpty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"openpty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __ia64__
-	{"openpty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"openpty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc__ && !__powerpc64__
-	{"openpty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"openpty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __powerpc64__
-	{"openpty","GLIBC_2.3",0,"GLIBC_2.3",4,1},
+	{"openpty","GLIBC_2.3",0,"LSB-Core",4,1},
 #endif
 #if __s390__ && !__s390x__
-	{"openpty","GLIBC_2.0",0,"GLIBC_2.0",4,1},
+	{"openpty","GLIBC_2.0",0,"LSB-Core",4,1},
 #endif
 #if __x86_64__
-	{"openpty","GLIBC_2.2.5",0,"GLIBC_2.2.5",4,1},
+	{"openpty","GLIBC_2.2.5",0,"LSB-Core",4,1},
 #endif
 #if __s390x__
-	{"openpty","GLIBC_2.2",0,"GLIBC_2.2",4,1},
+	{"openpty","GLIBC_2.2",0,"LSB-Core",4,1},
 #endif
 	{0,0}};
 
