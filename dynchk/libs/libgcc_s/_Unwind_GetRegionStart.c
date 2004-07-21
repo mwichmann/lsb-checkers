4cbad54199dc10430251d6466316e10ce14edc21
anderson <anderson>
2004-07-20 20:04:49 -0400

    Regen w/ latest script

--- dynchk/libs/libgcc_s/_Unwind_GetRegionStart.c
+++ dynchk/libs/libgcc_s/_Unwind_GetRegionStart.c
@@ -6,17 +6,21 @@
 #undef _Unwind_GetRegionStart
 static _Unwind_Ptr(*funcptr) () = 0;
 
+extern int __lsb_check_params;
+extern int __lsb_output(int, char*, ...);
 _Unwind_Ptr _Unwind_GetRegionStart ()
 {
+	int reset_flag = __lsb_check_params;
+	_Unwind_Ptr ret_value  ;
 	if(!funcptr)
 		funcptr = dlsym(RTLD_NEXT, "_Unwind_GetRegionStart");
-	return funcptr();
-}
-
-_Unwind_Ptr __lsb__Unwind_GetRegionStart ()
-{
-	if(!funcptr)
-		funcptr = dlsym(RTLD_NEXT, "_Unwind_GetRegionStart");
-	return funcptr();
+	if(__lsb_check_params)
+	{
+		__lsb_check_params=0;
+	__lsb_output(5-__lsb_check_params, "_Unwind_GetRegionStart()");
+	}
+	ret_value = funcptr();
+	__lsb_check_params = reset_flag;
+	return ret_value;
 }
 
