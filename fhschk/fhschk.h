b2bf87a67a05a9234e24e2e1d41f85574dd7b31d
mats <mats>
2004-07-28 13:34:54 -0400

    Add missing prototype to header

--- fhschk/fhschk.h
+++ fhschk/fhschk.h
@@ -3,6 +3,7 @@ extern void set_myappname(char *app);
 extern int is_fhs_readable(char *path);
 extern int is_fhs_writeble(char *path);
 extern int is_fhs_creatable(char *path);
+extern int is_fhs_installable(char *path);
 extern int is_fhs_execable(char *path);
 /* mkabs.c */
 extern char *mkabsolutepath(char *path);
