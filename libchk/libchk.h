3f1f5f8e94ae8ef0f35b14bdabc3a92c232f5f12
anderson <anderson>
2004-08-06 18:43:53 -0400

    First cut at actually checking symbol size for OBJT

--- libchk/libchk.h
+++ libchk/libchk.h
@@ -27,6 +27,7 @@ extern struct modlib modlibs[];
 extern int check_class_info(char *file, struct classinfo *classes[], struct tetj_handle *journal);
 /* libchk.c */
 extern int check_symbol(ElfFile *file, struct versym *entry);
+extern int check_size(ElfFile *file, struct versym *entry);
 extern void check_lib(char *libname, struct versym entries[], struct classinfo classes[], struct tetj_handle *journal);
 extern int main(int argc, char *argv[]);
 /* libs.c */
