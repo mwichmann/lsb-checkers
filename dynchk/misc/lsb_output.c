18c296c3db622a08c9e9f04feeabed408fd259c2
elderm <>
2004-10-25 13:45:09 -0400

    Changed so that initialization doesn't happen until libc has done enough initialization for getenv() to work properly.

--- dynchk/misc/lsb_output.c
+++ dynchk/misc/lsb_output.c
@@ -12,24 +12,24 @@
  * format, ... : output message, a la printf
  *
  * This is affected by the following environment variables:
- * LSB_OUTPUT_LEVEL : level below which we don't report output.
- * LDB_OUTPUT_FILE  : file to which output is sent.
+ * LSB_OUTPUT_LEVEL : level above which we don't report output.
+ * LSB_OUTPUT_FILE  : file to which output is sent.
  *
- * These environment variables should be set by options the lsbdynchk script.
+ * These environment variables can be set by options to the lsbdynchk script.
  */
+
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
-int __lsb_output(int level, char *format, ...)
+int __vlsb_output(int level, char *format, va_list args)
 {	
-	va_list args;
-	va_start(args, format);
-
 	static int need_init = 1;
 	static FILE * output;
 	static int out_level = 0;
+	static char prefix[10];
 	
 	char * filename = NULL;
 	char * level_name = NULL;
@@ -37,32 +37,57 @@ int __lsb_output(int level, char *format, ...)
 	{
 		need_init = 0;
 
+		// We *know* that LD_PRELOAD is being set, so if getenv()
+		// returns null on it, getenv() isn't ready yet.
+		if(!getenv("LD_PRELOAD"))
+		{
+		    need_init = 1;
+		    return 0;
+		}
 		filename = getenv("LSB_OUTPUT_FILE");
 		level_name = getenv("LSB_OUTPUT_LEVEL");
-
-		if(filename)
-		printf("DYNCHK DEBUG: filename = %s\n",filename);
-		if(level_name)
-		printf("DYNCHK DEBUG: level = %s\n", level_name);
 		
 		if(filename)
-			output = fopen(filename, "w");
+		{	output = fopen(filename, "w");
+			strcpy(prefix, "");
+		}
 		if(!output || !filename)
-			output = stderr;
+		{	output = stderr;
+			strcpy(prefix, "DYNCHK:");
+		}
 		if(level_name)
+		{
 			out_level = atoi(level_name);
-			
+		}
 	}
-
+//	printf("frotz(%d)\n",level);
 	if(level <= out_level)
 	{
 		if(out_level > 0)
-			fprintf(output, "DYNCHK(%d): ", level);
+			fprintf(output, "%s(%d): ", prefix, level);
 		else
-			fprintf(output, "DYNCHK: ");
+			fprintf(output, "%s ", prefix);
 		vfprintf(output, format, args);
 		fprintf(output, "\n");
 	}
-	va_end(args);
 	return 0;
+}
+
+int __lsb_output(int level, char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	int out = __vlsb_output(level, format, args);
+	va_end(args);
+	return out;
+}
+
+int __lsb_fail(char *name, char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	__lsb_output(-1, "Test failed at %s:", name);
+	int out = __vlsb_output(-1, format, args);
+	va_end(args);
+	return out;
 }
