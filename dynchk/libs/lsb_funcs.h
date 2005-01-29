a463091b14e1ee57153d5c2996ebc8a4824159e7
elderm <elderm>
2005-01-29 19:50:01 -0400

    Cleaning the tree: Removed some unused files.

--- dynchk/libs/lsb_funcs.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int lsb_fprintf(FILE * a0, const char * a1, ... );
-int lsb_fscanf(FILE * a0, const char * a1, ... );
-long lsb_sysconf(int a0);
-FILE *lsb_fopen(const char *,const char *);
-void * lsb_realloc(void *, size_t);
