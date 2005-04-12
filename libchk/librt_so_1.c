7a4f167a590bbe8a609a8d1a08a78b30ad4c55ab
anderson <anderson>
2005-04-11 22:43:26 -0400

    Make the clock_* func per arch

--- libchk/librt_so_1.c
+++ libchk/librt_so_1.c
@@ -1,11 +1,111 @@
 #include <stdlib.h>
 #include "elfchk.h"
 struct versym librt_so_1[] = {
-	{"clock_getcpuclockid","GLIBC_2.2",0,LSB_Core,0,1},
-	{"clock_getres","GLIBC_2.2",0,LSB_Core,0,1},
-	{"clock_gettime","GLIBC_2.2",0,LSB_Core,0,1},
-	{"clock_nanosleep","GLIBC_2.2",0,LSB_Core,0,1},
-	{"clock_settime","GLIBC_2.2",0,LSB_Core,0,1},
+#if __i386__
+	{"clock_getcpuclockid","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __ia64__
+	{"clock_getcpuclockid","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"clock_getcpuclockid","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc64__
+	{"clock_getcpuclockid","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390__ && !__s390x__
+	{"clock_getcpuclockid","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __x86_64__
+	{"clock_getcpuclockid","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390x__
+	{"clock_getcpuclockid","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __i386__
+	{"clock_getres","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __ia64__
+	{"clock_getres","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"clock_getres","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc64__
+	{"clock_getres","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390__ && !__s390x__
+	{"clock_getres","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __x86_64__
+	{"clock_getres","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390x__
+	{"clock_getres","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __i386__
+	{"clock_gettime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __ia64__
+	{"clock_gettime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"clock_gettime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc64__
+	{"clock_gettime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390__ && !__s390x__
+	{"clock_gettime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __x86_64__
+	{"clock_gettime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390x__
+	{"clock_gettime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __i386__
+	{"clock_nanosleep","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __ia64__
+	{"clock_nanosleep","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"clock_nanosleep","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc64__
+	{"clock_nanosleep","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390__ && !__s390x__
+	{"clock_nanosleep","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __x86_64__
+	{"clock_nanosleep","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390x__
+	{"clock_nanosleep","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __i386__
+	{"clock_settime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __ia64__
+	{"clock_settime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"clock_settime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __powerpc64__
+	{"clock_settime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390__ && !__s390x__
+	{"clock_settime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __x86_64__
+	{"clock_settime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
+#if __s390x__
+	{"clock_settime","GLIBC_2.2",0,LSB_Core,4,1},
+#endif
 	{"shm_open","GLIBC_2.2",0,LSB_Core,0,1},
 	{"shm_unlink","GLIBC_2.2",0,LSB_Core,0,1},
 	{"timer_create","GLIBC_2.2",0,LSB_Core,0,1},
