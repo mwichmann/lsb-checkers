e7a19ee33b9a7d40588b0b082beb604cbee6d5e9
mats <mats>
2005-05-04 21:05:05 -0400

    Finish cleanout now that journaling code moved to appchk.c

--- appchk/check_file.c
+++ appchk/check_file.c
@@ -23,43 +23,16 @@ void check_file(ElfFile *elffile, struct tetj_handle *journal,
   struct stat stat_info;
   int i;
   
-  /* XXX FIX ME - this now needs to be in appchk.c 
-  tetj_tp_count = 0;
-  tetj_testcase_start(journal, ++tetj_activity_count, filename, "");
-
-  tetj_tp_count++;
-  snprintf(tmp_string, TMP_STRING_SIZE, "Looking for file %s", filename);
-  tetj_purpose_start(journal, tetj_activity_count, tetj_tp_count, tmp_string);
-  */
-
-  /* Open ELF file for analysis */
-  if( elffile == NULL ) 
-  {
-    /* XXX FIX ME 
-    snprintf(tmp_string, TMP_STRING_SIZE, 
-             "Unable to open file %s as ELF binary\n", filename);
-    fprintf(stderr, tmp_string);
-    */
-    tetj_result(journal, tetj_activity_count, tetj_tp_count, TETJ_FAIL);
-    tetj_purpose_end(journal, tetj_activity_count, tetj_tp_count);
-    exit(1);
-  }
-  else
-  {
-    tetj_result(journal, tetj_activity_count, tetj_tp_count, TETJ_PASS);
-  }
-
   /* Log binary file size */
+  tetj_purpose_start(journal, tetj_activity_count, ++tetj_tp_count,
+		     "check file details");
   if (fstat(elffile->fd, &stat_info)==-1)
   {
-    /* XXX FIX ME 
-    snprintf(tmp_string, TMP_STRING_SIZE, "Could not stat file %s", 
-             filename);
+    snprintf(tmp_string, TMP_STRING_SIZE, "Could not stat file");
     perror(tmp_string);
-    */
     tetj_add_controller_error(journal, tmp_string);
-	free(elffile);
-    exit(1);
+    tetj_purpose_end(journal, tetj_activity_count, tetj_tp_count);
+    return;
   }
   snprintf(tmp_string, TMP_STRING_SIZE, "FILE_SIZE %lu", stat_info.st_size);
   tetj_testcase_info(journal, tetj_activity_count, tetj_tp_count, 0, 0, 0, tmp_string);
@@ -75,10 +48,11 @@ void check_file(ElfFile *elffile, struct tetj_handle *journal,
   tmp_string[32] = 0;
   snprintf(tmp_string2, TMP_STRING_SIZE, "BINARY_MD5SUM=%s", tmp_string);
   tetj_testcase_info(journal, tetj_activity_count, tetj_tp_count, 0, 0, 0, tmp_string2);
-
+  tetj_result(journal, tetj_activity_count, tetj_tp_count, TETJ_PASS);
   tetj_purpose_end(journal, tetj_activity_count, tetj_tp_count);
 
   checkElf(elffile, fileType, journal);
+  /* FIXME
   if (elffile->symhdr==NULL)
   {
     strncpy(tmp_string, 
@@ -87,6 +61,7 @@ void check_file(ElfFile *elffile, struct tetj_handle *journal,
     tetj_testcase_info(journal, tetj_activity_count, 0, 0, 0, 0, tmp_string);
     fprintf(stderr, tmp_string);
   }
+  */
   return;
 }
 
@@ -95,17 +70,6 @@ void check_lib(ElfFile *elffile, struct tetj_handle *journal, Elf_type fileType)
   int i;
   char tmp_string[TMP_STRING_SIZE+1];
   Elf_Shdr	*hdr1;
-
-  /* Open ELF file for analysis */
-  if( elffile == NULL ) 
-  {
-    /* XXX FIX ME
-    snprintf(tmp_string, TMP_STRING_SIZE, 
-             "Unable to open file %s as ELF binary\n", filename);
-    fprintf(stderr, tmp_string);
-    */
-    exit(1);
-  }
 
   /* Check all headers in extra lib */
   checkElfhdr(elffile, fileType, journal);
