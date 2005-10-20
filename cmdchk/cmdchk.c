131585d3cc38a8c3a02281ccd04a5634f1cc3e37
pradosh <pradosh>
2005-10-20 12:57:48 -0400

    This commit contains only ndentation using "indent -kr -sob" There are NO code changes

--- cmdchk/cmdchk.c
+++ cmdchk/cmdchk.c
@@ -5,9 +5,13 @@
  *
  * Stuart Anderson (anderson@freestandards.org)
  *
- * This is $Revision: 1.18 $
+ * This is $Revision: 1.19 $
  *
  * $Log: cmdchk.c,v $
+ * Revision 1.19  2005/10/20 12:57:48  pradosh
+ * This commit contains only ndentation using "indent -kr -sob"
+ * There are NO code changes
+ *
  * Revision 1.18  2005/07/06 22:33:31  mats
  * Add invocation command line to cmdchk journal (bug 1041)
  *
@@ -93,10 +97,10 @@ char *binpaths[] = {
 char prefix[TMP_STRING_SIZE + 1];
 
 /* Real CVS revision number so we can strings it from the binary if necessary */
-static const char *__attribute((unused)) cmdchk_revision = "$Revision: 1.18 $";
+static const char *__attribute((unused)) cmdchk_revision =
+    "$Revision: 1.19 $";
 
-void
-check_cmd(struct cmds *cp, struct tetj_handle *journal)
+void check_cmd(struct cmds *cp, struct tetj_handle *journal)
 {
     char filename[PATH_MAX + 1];
     char tmp_string[TMP_STRING_SIZE + 1];
@@ -105,8 +109,10 @@ check_cmd(struct cmds *cp, struct tetj_handle *journal)
 
     tetj_tp_count = 1;
     tetj_testcase_start(journal, tetj_activity_count, cp->cmdname, "");
-    snprintf(tmp_string, TMP_STRING_SIZE, "Looking for command %s", cp->cmdname);
-    tetj_purpose_start(journal, tetj_activity_count, tetj_tp_count, tmp_string);
+    snprintf(tmp_string, TMP_STRING_SIZE, "Looking for command %s",
+	     cp->cmdname);
+    tetj_purpose_start(journal, tetj_activity_count, tetj_tp_count,
+		       tmp_string);
 
     /*
      * TODO: LSB bug 1006 - once command paths are cleaned out in DB,
@@ -117,11 +123,13 @@ check_cmd(struct cmds *cp, struct tetj_handle *journal)
     /* Find the command */
     for (i = 0; binpaths[i]; i++) {
 	if (prefix)
-	    snprintf(filename, PATH_MAX, "%s%s%s", prefix, binpaths[i], cp->cmdname);
+	    snprintf(filename, PATH_MAX, "%s%s%s", prefix, binpaths[i],
+		     cp->cmdname);
 	else
 	    snprintf(filename, PATH_MAX, "%s%s", binpaths[i], cp->cmdname);
 	if (access(filename, X_OK) == 0) {
-	    tetj_result(journal, tetj_activity_count, tetj_tp_count, TETJ_PASS);
+	    tetj_result(journal, tetj_activity_count, tetj_tp_count,
+			TETJ_PASS);
 #ifdef VERBOSE
 	    fprintf(stderr, "Found %s as %s\n", cp->cmdname, filename);
 #endif
@@ -130,7 +138,8 @@ check_cmd(struct cmds *cp, struct tetj_handle *journal)
 
 	if (stat(filename, &stbuf) == 0) {
 	    if (stbuf.st_mode & S_IXUSR) {
-		tetj_result(journal, tetj_activity_count, tetj_tp_count, TETJ_PASS);
+		tetj_result(journal, tetj_activity_count, tetj_tp_count,
+			    TETJ_PASS);
 #ifdef VERBOSE
 		fprintf(stderr, "Found %s as %s\n", cp->cmdname, filename);
 #endif
@@ -140,17 +149,17 @@ check_cmd(struct cmds *cp, struct tetj_handle *journal)
     }
     if (!binpaths[i]) {
 	fprintf(stderr, "Couldn't find %s\n", cp->cmdname);
-	tetj_result(journal, tetj_activity_count, tetj_tp_count, TETJ_FAIL);
+	tetj_result(journal, tetj_activity_count, tetj_tp_count,
+		    TETJ_FAIL);
     }
     tetj_purpose_end(journal, tetj_activity_count, tetj_tp_count);
     tetj_testcase_end(journal, tetj_activity_count++, 0, "");
 }
 
-char *
-concat_string(char *input, char *addition)
+char *concat_string(char *input, char *addition)
 {
     char *tmp;
-    if (input) { 
+    if (input) {
 	tmp = realloc(input, strlen(input) + strlen(addition) + 1);
 	if (!tmp)
 	    abort();
@@ -160,19 +169,17 @@ concat_string(char *input, char *addition)
     }
 }
 
-void
-usage(char *progname)
+void usage(char *progname)
 {
-  printf("usage: %s [options]\n%s%s%s%s%s", progname,
-"  -h, --help                     show this help message and exit\n",
-"  -v, --version                  show version and LSB version\n",
-"  -n, --nojournal                do not write a journal file\n",
-"  -j JOURNAL, --version=JOURNAL  use JOURNAL as file/path for journal file\n",
-"  -p PREFIX, --prefix=PREFIX     prefix to append to all paths\n");
+    printf("usage: %s [options]\n%s%s%s%s%s", progname,
+	   "  -h, --help                     show this help message and exit\n",
+	   "  -v, --version                  show version and LSB version\n",
+	   "  -n, --nojournal                do not write a journal file\n",
+	   "  -j JOURNAL, --version=JOURNAL  use JOURNAL as file/path for journal file\n",
+	   "  -p PREFIX, --prefix=PREFIX     prefix to append to all paths\n");
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     struct tetj_handle *journal;
     char *command_line = NULL;
@@ -190,63 +197,67 @@ main(int argc, char *argv[])
     while (1) {
 	int c;
 	static struct option long_options[] = {
-	    {"help",     no_argument,        NULL, 'h'},
-	    {"version",  no_argument,        NULL, 'v'},
-	    {"nojournal",no_argument,        NULL, 'n'},
-	    {"journal",  required_argument,  NULL, 'j'},
-	    {"prefix",   required_argument,  NULL, 'p'},
+	    {"help", no_argument, NULL, 'h'},
+	    {"version", no_argument, NULL, 'v'},
+	    {"nojournal", no_argument, NULL, 'n'},
+	    {"journal", required_argument, NULL, 'j'},
+	    {"prefix", required_argument, NULL, 'p'},
 	    {0, 0, 0, 0}
 	};
 
-	c = getopt_long (argc, argv, "hvnj:p:", long_options, &option_index);
+	c = getopt_long(argc, argv, "hvnj:p:", long_options,
+			&option_index);
 	if (c == -1)
 	    break;
 	switch (c) {
-	    case 'h':
-		usage(argv[0]);
-		exit (0);
-	    case 'v':
-		printf("%s %s for LSB Specification %s\n", argv[0],
-		       LSBCMDCHK_VERSION, LSBVERSION);
-		break;
-	    case 'n':
-		snprintf(journal_filename, TMP_STRING_SIZE, "/dev/null");
-		break;
-	    case 'j':
-		snprintf(journal_filename, TMP_STRING_SIZE, optarg);
-		break;
-	    case 'p':
-		strcpy(prefix, optarg);
-		break;
-	    default:
-		usage(argv[0]);
-		exit (0);
+	case 'h':
+	    usage(argv[0]);
+	    exit(0);
+	case 'v':
+	    printf("%s %s for LSB Specification %s\n", argv[0],
+		   LSBCMDCHK_VERSION, LSBVERSION);
+	    break;
+	case 'n':
+	    snprintf(journal_filename, TMP_STRING_SIZE, "/dev/null");
+	    break;
+	case 'j':
+	    snprintf(journal_filename, TMP_STRING_SIZE, optarg);
+	    break;
+	case 'p':
+	    strcpy(prefix, optarg);
+	    break;
+	default:
+	    usage(argv[0]);
+	    exit(0);
 	}
     }
     if (optind != argc) {
 	usage(argv[0]);
-	exit (1);
-    }
-
-    if (tetj_start_journal(journal_filename, &journal, command_line) != 0) {
-	snprintf(tmp_string, TMP_STRING_SIZE, "Could not open journal file %s",
-		 journal_filename);
-	perror(tmp_string);
-	fprintf(stderr, "Use -j <filename> to specify an alternate location for the journal file\n");
 	exit(1);
     }
 
-    snprintf(tmp_string, TMP_STRING_SIZE, "VSX_NAME=lsbcmdchk " LSBCMDCHK_VERSION);
+    if (tetj_start_journal(journal_filename, &journal, command_line) != 0) {
+	snprintf(tmp_string, TMP_STRING_SIZE,
+		 "Could not open journal file %s", journal_filename);
+	perror(tmp_string);
+	fprintf(stderr,
+		"Use -j <filename> to specify an alternate location for the journal file\n");
+	exit(1);
+    }
+
+    snprintf(tmp_string, TMP_STRING_SIZE,
+	     "VSX_NAME=lsbcmdchk " LSBCMDCHK_VERSION);
     tetj_add_config(journal, tmp_string);
     snprintf(tmp_string, TMP_STRING_SIZE, "LSB_VERSION= " LSBVERSION);
     tetj_add_config(journal, tmp_string);
     tetj_config_end(journal);
 
-    j = sizeof(cmdlist)/sizeof(struct cmds);
-    snprintf(tmp_string, TMP_STRING_SIZE, "\"total tests in cmdchk %d\"", j);
+    j = sizeof(cmdlist) / sizeof(struct cmds);
+    snprintf(tmp_string, TMP_STRING_SIZE, "\"total tests in cmdchk %d\"",
+	     j);
     tetj_scenario_info(journal, tmp_string);
 
-    for (i=0; i < j; i++)
+    for (i = 0; i < j; i++)
 	check_cmd(&cmdlist[i], journal);
     tetj_close_journal(journal);
     exit(0);
