df81e7703ba5d0fb66f4d4714922b11529005699
mats <>
2006-01-17 17:27:26 -0400

    syncing in generated files to match earlier changes

---
+++ elfchk/modules.c
@@ -0,0 +1,10 @@
+/* GENERATED FILE, DO NOT MODIFY */
+#include "modules.h"
+
+struct lsb_module LSB_Modules[] = {
+	{"LSB_Graphics",	LSB_Graphics},
+	{"LSB_Desktop",	LSB_Desktop},
+	{"LSB_Cpp",	LSB_Cpp},
+	{"LSB_Core",	LSB_Core},
+	{NULL, 0},
+};
