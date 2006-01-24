e14461ccd7856c656af51333f379e074adab125b
heffler <heffler>
2006-01-24 17:04:11 -0400

    sync with latest DB changes

--- elfchk/modules.c
+++ elfchk/modules.c
@@ -2,8 +2,11 @@
 #include "modules.h"
 
 struct lsb_module LSB_Modules[] = {
+	{"LSB_XML",	LSB_XML},
+	{"LSB_Toolkit_Qt",	LSB_Toolkit_Qt},
+	{"LSB_Toolkit_Gtk",	LSB_Toolkit_Gtk},
+	{"LSB_Graphics_Ext",	LSB_Graphics_Ext},
 	{"LSB_Graphics",	LSB_Graphics},
-	{"LSB_Desktop",	LSB_Desktop},
 	{"LSB_Cpp",	LSB_Cpp},
 	{"LSB_Core",	LSB_Core},
 	{NULL, 0},
--- elfchk/modules.h
+++ elfchk/modules.h
@@ -12,8 +12,11 @@ extern struct lsb_module LSB_Modules[];
 
 #define LSB_Core	0x1
 #define LSB_Cpp	0x2
-#define LSB_Desktop	0x4
-#define LSB_Graphics	0x8
+#define LSB_Graphics	0x4
+#define LSB_Graphics_Ext	0x8
+#define LSB_Toolkit_Gtk	0x10
+#define LSB_Toolkit_Qt	0x20
+#define LSB_XML	0x40
 #define LSB_All_Modules 0xff
 
 #endif
