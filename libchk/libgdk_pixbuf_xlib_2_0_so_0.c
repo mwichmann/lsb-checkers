16db95ef0b3c840f2cb174e68b876eab6fae7872
mats <mats>
2006-03-15 14:48:20 -0400

    adding renamed (generated) file from but 1287

---
+++ libchk/libgdk_pixbuf_xlib_2_0_so_0.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "elfchk.h"
+struct versym libgdk_pixbuf_xlib_2_0_so_0[] = {
+	{"gdk_pixbuf_xlib_get_from_drawable","",0,LSB_Toolkit_Gtk,0,1},
+	{"gdk_pixbuf_xlib_init","",0,LSB_Toolkit_Gtk,0,1},
+	{"gdk_pixbuf_xlib_init_with_depth","",0,LSB_Toolkit_Gtk,0,1},
+	{"gdk_pixbuf_xlib_render_pixmap_and_mask","",0,LSB_Toolkit_Gtk,0,1},
+	{"gdk_pixbuf_xlib_render_threshold_alpha","",0,LSB_Toolkit_Gtk,0,1},
+	{"gdk_pixbuf_xlib_render_to_drawable","",0,LSB_Toolkit_Gtk,0,1},
+	{"gdk_pixbuf_xlib_render_to_drawable_alpha","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_draw_gray_image","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_draw_indexed_image","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_draw_rgb_32_image","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_draw_rgb_image","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_draw_rgb_image_dithalign","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_cmap_free","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_cmap_new","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_ditherable","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_gc_set_background","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_gc_set_foreground","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_get_cmap","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_get_depth","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_get_display","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_get_screen","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_get_visual","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_get_visual_info","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_init","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_init_with_depth","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_set_install","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_set_min_colors","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_set_verbose","",0,LSB_Toolkit_Gtk,0,1},
+	{"xlib_rgb_xpixel_from_rgb","",0,LSB_Toolkit_Gtk,0,1},
+	{0,0}};
+
+struct classinfo *libgdk_pixbuf_xlib_2_0_so_0_classinfo[] = {
+
+	NULL	};
