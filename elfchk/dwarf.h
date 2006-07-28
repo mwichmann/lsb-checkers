a157eaf831e865a9287ce9a37da47bdcc12c3293
mats <mats>
2006-07-27 20:09:29 -0400

    indent -kr for programmer sanity

--- elfchk/dwarf.h
+++ elfchk/dwarf.h
@@ -11,54 +11,54 @@
 #include <elf.h>
 
 typedef struct CIEFrameImage {
-	Elf32_Word	length;
-	Elf32_Word	cie;
-	char		version;
-	char		augmentation[1];
-	} CIEFrameImage;
+    Elf32_Word length;
+    Elf32_Word cie;
+    char version;
+    char augmentation[1];
+} CIEFrameImage;
 
 typedef struct CIEFrame {
     struct CIEFrame *next;
-	unsigned char *cie_start_addr;
-	unsigned long	length;
-	Elf32_Word	cie;
-	char		version;
-	char		per_encoding;
-	char		lsda_encoding;
-	char		fde_encoding;
-	char		*augmentation;
-	int		code_alignment_factor;
-	int		data_alignment_factor;
-	int		return_address_register;
-	int		augmentation_len;
-	int		initial_instructions;
-	unsigned char	*personality_routine;
-	} CIEFrame;
+    unsigned char *cie_start_addr;
+    unsigned long length;
+    Elf32_Word cie;
+    char version;
+    char per_encoding;
+    char lsda_encoding;
+    char fde_encoding;
+    char *augmentation;
+    int code_alignment_factor;
+    int data_alignment_factor;
+    int return_address_register;
+    int augmentation_len;
+    int initial_instructions;
+    unsigned char *personality_routine;
+} CIEFrame;
 
 typedef struct FDEFrameHeader {
-	unsigned long	length;
-	Elf32_Word	CIE_pointer;
-	long		*initial_location;
-	long		address_range;
-	int		augmentation_len;
-	unsigned char	*augmentation_data;
-	} FDEFrameHeader;
+    unsigned long length;
+    Elf32_Word CIE_pointer;
+    long *initial_location;
+    long address_range;
+    int augmentation_len;
+    unsigned char *augmentation_data;
+} FDEFrameHeader;
 
 typedef struct EHFRMHDRImage {
-	unsigned char	version;
-	unsigned char	eh_frame_ptr_enc;
-	unsigned char	fde_count_enc;
-	unsigned char	table_enc;
-	} EHFRMHDRImage;
+    unsigned char version;
+    unsigned char eh_frame_ptr_enc;
+    unsigned char fde_count_enc;
+    unsigned char table_enc;
+} EHFRMHDRImage;
 
 typedef struct EHFRMHDR {
-	unsigned char	version;
-	unsigned char	eh_frame_ptr_enc;
-	unsigned char	fde_count_enc;
-	unsigned char	table_enc;
-	void		*eh_frame_ptr;
-	long		fde_count;
-	} EHFRMHDR;
+    unsigned char version;
+    unsigned char eh_frame_ptr_enc;
+    unsigned char fde_count_enc;
+    unsigned char table_enc;
+    void *eh_frame_ptr;
+    long fde_count;
+} EHFRMHDR;
 
 /* DWARF Call Frame Instruction - Taken from DWARF v2.0.0 */
 
@@ -120,11 +120,13 @@ typedef struct EHFRMHDR {
 
 
 /* dwarf.c */
-extern unsigned long int read_leb128(unsigned char *data, int *length_return, int sign);
+extern unsigned long int read_leb128(unsigned char *data,
+				     int *length_return, int sign);
 extern int check_CFI(unsigned char *ptr, int *error, int ptr_encoding);
-extern int check_FDE(CIEFrame *cie_list, unsigned char *ptr, int *error);
-extern int check_CIE(CIEFrame **cie_list, unsigned char *ptr, int *error);
+extern int check_FDE(CIEFrame * cie_list, unsigned char *ptr, int *error);
+extern int check_CIE(CIEFrame ** cie_list, unsigned char *ptr, int *error);
 extern int check_CFInformation(unsigned char *ptr, int *error);
-extern void *read_FDE_encoded(unsigned char *ptr, unsigned char encoding, int *numused);
+extern void *read_FDE_encoded(unsigned char *ptr, unsigned char encoding,
+			      int *numused);
 
 #endif /* _DWARF_H */
