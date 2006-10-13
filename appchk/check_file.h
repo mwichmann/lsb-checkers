1781bb8e970364d87c14a445940963debd154803
Jeff Licquia <jeff@licquia.org>
2006-10-13 13:46:34 -0400

    Fix check_file and check_lib to use the new macros.

--- appchk/check_file.h
+++ appchk/check_file.h
@@ -1,12 +1,11 @@
 #ifndef __CHECK_FILE_H
 #define __CHECK_FILE_H
 
-#include "../tetj/tetj.h"
 #include "../elfchk/elfchk.h"
 #include "../elfchk/hdr.h"
 #include "../elfchk/proghdr.h"
 
-int check_file(ElfFile *elffile, struct tetj_handle *journal, Elf_type isLib);
-void check_lib(ElfFile *elffile, struct tetj_handle *journal, Elf_type isLib, int modules);
+int check_file(ElfFile *elffile, Elf_type isLib);
+void check_lib(ElfFile *elffile, Elf_type isLib, int modules);
 
 #endif /* __CHECK_FILE_H */
