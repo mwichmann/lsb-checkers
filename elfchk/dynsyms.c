da7bb785d0a0b6ee4b82f0937aeead2dfbca713c
Mats Wichmann <mats@freestandards.org>
2007-05-26 15:39:26 -0600

    regenerate with correct symbol versions for posix_spawn (bug 1390)

--- elfchk/dynsyms.c
+++ elfchk/dynsyms.c
@@ -66315,27 +66315,447 @@ struct versym DynSyms[] = {
 #if __s390x__
 	{"posix_openpt","GLIBC_2.2.1",0,LSB_Core},
 #endif
+#if __i386__
 	{"posix_spawn","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawn","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawn","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawn","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawn","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawn","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawn","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawn_file_actions_addclose","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawn_file_actions_addclose","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawn_file_actions_addclose","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawn_file_actions_addclose","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawn_file_actions_addclose","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawn_file_actions_addclose","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawn_file_actions_addclose","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawn_file_actions_adddup2","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawn_file_actions_adddup2","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawn_file_actions_adddup2","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawn_file_actions_adddup2","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawn_file_actions_adddup2","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawn_file_actions_adddup2","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawn_file_actions_adddup2","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawn_file_actions_addopen","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawn_file_actions_addopen","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawn_file_actions_addopen","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawn_file_actions_addopen","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawn_file_actions_addopen","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawn_file_actions_addopen","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawn_file_actions_addopen","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawn_file_actions_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawn_file_actions_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawn_file_actions_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawn_file_actions_destroy","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawn_file_actions_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawn_file_actions_destroy","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawn_file_actions_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawn_file_actions_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawn_file_actions_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawn_file_actions_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawn_file_actions_init","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawn_file_actions_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawn_file_actions_init","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawn_file_actions_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_destroy","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_destroy","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_destroy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_getflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_getflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_getflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_getflags","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_getflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_getflags","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_getflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_getpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_getpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_getpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_getpgroup","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_getpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_getpgroup","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_getpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_getschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_getschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_getschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_getschedparam","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_getschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_getschedparam","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_getschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_getschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_getschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_getschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_getschedpolicy","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_getschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_getschedpolicy","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_getschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_getsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_getsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_getsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_getsigdefault","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_getsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_getsigdefault","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_getsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_getsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_getsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_getsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_getsigmask","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_getsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_getsigmask","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_getsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_init","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_init","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_init","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_setflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_setflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_setflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_setflags","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_setflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_setflags","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_setflags","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_setpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_setpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_setpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_setpgroup","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_setpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_setpgroup","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_setpgroup","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_setschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_setschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_setschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_setschedparam","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_setschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_setschedparam","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_setschedparam","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_setschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_setschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_setschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_setschedpolicy","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_setschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_setschedpolicy","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_setschedpolicy","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_setsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_setsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_setsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_setsigdefault","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_setsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_setsigdefault","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_setsigdefault","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnattr_setsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnattr_setsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnattr_setsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnattr_setsigmask","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnattr_setsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnattr_setsigmask","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnattr_setsigmask","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __i386__
 	{"posix_spawnp","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __ia64__
+	{"posix_spawnp","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc__ && !__powerpc64__
+	{"posix_spawnp","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __powerpc64__
+	{"posix_spawnp","GLIBC_2.3",0,LSB_Core},
+#endif
+#if __s390__ && !__s390x__
+	{"posix_spawnp","GLIBC_2.2",0,LSB_Core},
+#endif
+#if __x86_64__
+	{"posix_spawnp","GLIBC_2.2.5",0,LSB_Core},
+#endif
+#if __s390x__
+	{"posix_spawnp","GLIBC_2.2",0,LSB_Core},
+#endif
 #if __i386__
 	{"pow","GLIBC_2.0",0,LSB_Core},
 #endif
