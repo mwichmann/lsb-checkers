03b531cd0eee60903b96a21082faeb5af0f817ed
Mats Wichmann <mats@freestandards.org>
2007-06-13 08:24:27 -0600

    apply devchk patches for bug 1658

--- devchk/ts/devchk/hdrchk.h
+++ devchk/ts/devchk/hdrchk.h
@@ -21,72 +21,72 @@ int Log(char *f, ...);
 /*
  * Utility macros used by the header tests
  */
-#define CompareConstant(const,value,cid,aid) \
+#define CompareConstant(const,value,cid,aid,appearedin,withdrawnin) \
 	cnt++; \
 	Log("subtest %d\n", cnt); \
 	Log("Purpose: Compare Constant "#const" has value  %d\n", value); \
 	if( const != value ) {\
 		Msg(#const " is %d instead of expected %d\n", const, value); \
-		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue) VALUES(%d," #cid ",%d);\n", aid, const);  \
+		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue,ACappearedin,ACwithdrawnin) VALUES(%d," #cid ",%d, '"#appearedin"', "#withdrawnin");\n", aid, const);  \
 		HDRCHKTEST_FAIL \
 	} \
 	else { \
 		HDRCHKTEST_PASS \
 	} 
 
-#define CompareLongConstant(const,value,cid,aid) \
+#define CompareLongConstant(const,value,cid,aid,appearedin,withdrawnin) \
 	cnt++; \
 	Log("subtest %d\n", cnt); \
 	Log("Purpose: Compare Constant "#const" has value  %ld\n", value); \
 	if( (long)const != value ) {\
 		Msg(#const " is %ld instead of expected %ld\n", const, value); \
-		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue) VALUES(%d," #cid ",%ld);\n", aid, const);  \
+		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue,ACappearedin,ACwithdrawnin) VALUES(%d," #cid ",%ld, '"#appearedin"', "#withdrawnin");\n", aid, const);  \
 		HDRCHKTEST_FAIL \
 	} \
 	else { \
 		HDRCHKTEST_PASS \
 	} 
 
-#define CompareFloatConstant(const,value,cid,aid) \
+#define CompareFloatConstant(const,value,cid,aid,appearedin,withdrawnin) \
 	cnt++; \
 	Log("subtest %d\n", cnt); \
 	Log("Purpose: Compare Constant "#const" has value  %f\n", value); \
 	if( (float)const != value ) {\
 		Msg(#const " is %f instead of expected %f\n", const, value); \
-		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue) VALUES(%d," #cid ",%f);\n", aid, const);  \
+		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue,ACappearedin,ACwithdrawnin) VALUES(%d," #cid ",%f, '"#appearedin"', "#withdrawnin");\n", aid, const);  \
 		HDRCHKTEST_FAIL \
 	} \
 	else { \
 		HDRCHKTEST_PASS \
 	} 
 
-#define CompareDoubleConstant(const,value,cid,aid) \
+#define CompareDoubleConstant(const,value,cid,aid,appearedin,withdrawnin) \
 	cnt++; \
 	Log("subtest %d\n", cnt); \
 	Log("Purpose: Compare Constant "#const" has value  %a\n", value); \
 	if( (double)const != value ) {\
 		Msg(#const " is %a instead of expected %a\n", const, value); \
-		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue) VALUES(%d," #cid ",%a);\n", aid, const);  \
+		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue,ACappearedin,ACwithdrawnin) VALUES(%d," #cid ",%a, '"#appearedin"', "#withdrawnin");\n", aid, const);  \
 		HDRCHKTEST_FAIL \
 	} \
 	else { \
 		HDRCHKTEST_PASS \
 	} 
 
-#define CompareLongDoubleConstant(const,value,cid,aid) \
+#define CompareLongDoubleConstant(const,value,cid,aid,appearedin,withdrawnin) \
 	cnt++; \
 	Log("subtest %d\n", cnt); \
 	Log("Purpose: Compare Constant "#const" has value  %La\n", value); \
 	if( (long double)const != value ) {\
 		Msg(#const " is %La instead of expected %La\n", const, value); \
-		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue) VALUES(%d," #cid ",%La);\n", aid, const);  \
+		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue,ACappearedin,ACwithdrawnin) VALUES(%d," #cid ",%La, '"#appearedin"', "#withdrawnin");\n", aid, const);  \
 		HDRCHKTEST_FAIL \
 	} \
 	else { \
 		HDRCHKTEST_PASS \
 	} 
 
-#define CompareStringConstant(const,value) \
+#define CompareStringConstant(const,value,cid,aid,appearedin,withdrawnin) \
 	cnt++; \
 	Log("subtest %d\n", cnt); \
 	Log("Purpose: Compare String Constant "#const" has value  %s\n", value); \
@@ -94,7 +94,7 @@ int Log(char *f, ...);
 		Msg(#const " is %s instead of expected %s\n", const, value); \
 		/* Msg(#const " is %f instead of expected %f\n", const, value); \
 		Msg(#const " is %g instead of expected %g\n", const, value); \
-		Msg("UPDATE Constants SET Cvalue=%s WHERE Cname='" #const "';\n", const); */ \
+		Msg("REPLACE INTO ArchConst (ACaid,ACcid,ACvalue,ACappearedin,ACwithdrawnin) VALUES(%d, #cid, %s, '"#appearedin"', "#withdrawnin");\n", aid, const); */ \
 		HDRCHKTEST_FAIL \
 	} \
 	else { \
@@ -102,14 +102,14 @@ int Log(char *f, ...);
 	} 
 
 
-#define CheckTypeSize(type,size,tid,aid) \
+#define CheckTypeSize(type,size,tid,aid,appearedin,withdrawnin,basetype,attribute) \
 	cnt++; \
 	Log("subtest %d\n", cnt); \
 	Log("Purpose: Check sizeof( "#type" ) is %d\n", size); \
 	if( sizeof(type) != size ) { \
 		Msg("sizeof(" #type ") found %d instead of " #size "\n",sizeof(type)); \
-		Msg("REPLACE INTO ArchType (ATtid,ATaid,ATsize) "); \
-		Msg("VALUES (%d, %d, %d);\n", tid, aid, sizeof(type)); \
+		Msg("REPLACE INTO ArchType (ATtid,ATaid,ATsize,ATappearedin,ATwithdrawnin,ATbasetype,ATattribute) "); \
+    		Msg("VALUES (%d, %d, %d, '"#appearedin"', "#withdrawnin", %d, "#attribute");\n", tid, aid, sizeof(type), basetype); \
 		HDRCHKTEST_FAIL \
 	} \
 	else { \
@@ -126,7 +126,7 @@ int Log(char *f, ...);
 	Log("Purpose: Check sizeof of ( foo."#member ") is %d\n", size); \
 	if( sizeof(foo.member) != size ) { \
 		Msg("sizeof( " #str "." #member ") found %d instead of " #size "\n",(sizeof(foo.member))); \
-		Msg("REPLACE INTO ArchTypeMem (ATMaid,ATMtmid,ATMsize,ATMoffset) "); \
+		Msg("REPLACE INTO TypeMemberExtras (TMEaid,TMEtmid,TMEsize,TMEoffset) "); \
 		Msg("VALUES (%d,%d,%d,%d );\n",aid,tmid, (sizeof(foo.member)),(foom-foob)); \
 		HDRCHKTEST_FAIL \
 	} \
@@ -145,7 +145,7 @@ int Log(char *f, ...);
 	Log("Purpose: Check offset of (" #member ") is %d\n", offset); \
 	if( (foom-foob) != offset ) { \
 		Msg("offset(" #str "." #member ") found %d instead of " #offset "\n",(foom-foob)); \
-		Msg("REPLACE INTO ArchTypeMem (ATMaid,ATMtmid,ATMsize,ATMoffset) "); \
+		Msg("REPLACE INTO TypeMemberExtras (TMEaid,TMEtmid,TMEsize,TMEoffset) "); \
 		Msg("VALUES (%d,%d,%d,%d );\n",aid,tmid, (sizeof(foo.member)),(foom-foob)); \
 		HDRCHKTEST_FAIL \
 	} \
