a0a2022b2d2ac0f8d8248af68fcac62703abd69c
Mats Wichmann <mats@freestandards.org>
2007-10-20 11:20:55 -0600

    regen devchk after S390, S390X db updates

--- devchk/ts/devchk/freetype_ftlist_h.c
+++ devchk/ts/devchk/freetype_ftlist_h.c
@@ -32,6 +32,10 @@ printf("Checking data structures in freetype/ftlist.h\n");
 CheckTypeSize(FT_List_Destructor,8, 16736, 11, 3.2, NULL, 16735, NULL)
 #elif defined __i386__
 CheckTypeSize(FT_List_Destructor,4, 16736, 2, 3.2, NULL, 16735, NULL)
+#elif defined __s390__ && !defined __s390x__
+CheckTypeSize(FT_List_Destructor,0, 16736, 10, 3.2, NULL, 16735, NULL)
+#elif defined __s390x__
+CheckTypeSize(FT_List_Destructor,0, 16736, 12, 3.2, NULL, 16735, NULL)
 #else
 Msg("REPLACE INTO ArchType VALUES (%d,%d,%d,'""3.2""',NULL,16735,NULL);\n",architecture,16736,0);
 Msg("Find size of FT_List_Destructor (16736)\n");
@@ -41,6 +45,10 @@ Msg("Find size of FT_List_Destructor (16736)\n");
 CheckTypeSize(FT_List_Iterator,8, 16739, 11, 3.2, NULL, 16738, NULL)
 #elif defined __i386__
 CheckTypeSize(FT_List_Iterator,4, 16739, 2, 3.2, NULL, 16738, NULL)
+#elif defined __s390__ && !defined __s390x__
+CheckTypeSize(FT_List_Iterator,0, 16739, 10, 3.2, NULL, 16738, NULL)
+#elif defined __s390x__
+CheckTypeSize(FT_List_Iterator,0, 16739, 12, 3.2, NULL, 16738, NULL)
 #else
 Msg("REPLACE INTO ArchType VALUES (%d,%d,%d,'""3.2""',NULL,16738, NULL);\n",architecture,16739,0);
 Msg("Find size of FT_List_Iterator (16739)\n");
