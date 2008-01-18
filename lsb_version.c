92a005c7dce368819669911053cfeb5070353725
Mats Wichmann <mats@freestandards.org>
2008-01-18 09:58:08 -0700

    make 3.2 the default version now

--- lsb_version.c
+++ lsb_version.c
@@ -4,5 +4,5 @@ int num_LSB_Versions = sizeof(LSB_Versions)/sizeof(char *);
 
 int LSB_Version = -1;
 
-/* Default LSB version is 3.1 */
-int LSB_Version_default = 1;
+/* Default LSB version is 3.2 */
+int LSB_Version_default = 2;
