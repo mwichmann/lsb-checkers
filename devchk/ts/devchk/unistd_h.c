e49bb45fe6ac6e64f373fded107af89556a1f096
Mats Wichmann <mats@freestandards.org>
2008-05-23 06:49:00 -0600

    adjust some headers for native building (bug 2083)

--- devchk/ts/devchk/unistd_h.c
+++ devchk/ts/devchk/unistd_h.c
@@ -6,6 +6,12 @@
 #include <sys/types.h>
 #define _LSB_DEFAULT_ARCH 1
 #define __LSB_VERSION__ 40
+#ifndef LSBCC_MODE
+#ifdef __USE_FORTIFY_LEVEL
+#undef __USE_FORTIFY_LEVEL
+#endif
+#define __USE_FORTIFY_LEVEL 2
+#endif
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
