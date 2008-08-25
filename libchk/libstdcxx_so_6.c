28bb49465b4f00db1fa51d625c978717028fd24a
Mats Wichmann <mats@freestandards.org>
2008-08-25 10:37:22 -0600

    fold in patches from bugs 706, 2224 and 2238

--- libchk/libstdcxx_so_6.c
+++ libchk/libstdcxx_so_6.c
@@ -5438,6 +5438,9 @@ struct versym libstdcxx_so_6[] = {
 #if defined __i386__
 	{"_ZNSt10__num_base11_S_atoms_inE","GLIBCXX_3.4",0,LSB_Cpp,4,0},
 #endif
+#if defined __ia64__
+	{"_ZNSt10__num_base11_S_atoms_inE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
+#endif
 #if defined __powerpc__ && !defined __powerpc64__
 	{"_ZNSt10__num_base11_S_atoms_inE","GLIBCXX_3.4",0,LSB_Cpp,4,0},
 #endif
@@ -5447,18 +5450,18 @@ struct versym libstdcxx_so_6[] = {
 #if defined __s390__ && !defined __s390x__
 	{"_ZNSt10__num_base11_S_atoms_inE","GLIBCXX_3.4",0,LSB_Cpp,4,0},
 #endif
-#if defined __s390x__
-	{"_ZNSt10__num_base11_S_atoms_inE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
-#endif
-#if defined __ia64__
-	{"_ZNSt10__num_base11_S_atoms_inE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
-#endif
 #if defined __x86_64__
+	{"_ZNSt10__num_base11_S_atoms_inE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
+#endif
+#if defined __s390x__
 	{"_ZNSt10__num_base11_S_atoms_inE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
 #endif
 #if defined __i386__
 	{"_ZNSt10__num_base12_S_atoms_outE","GLIBCXX_3.4",0,LSB_Cpp,4,0},
 #endif
+#if defined __ia64__
+	{"_ZNSt10__num_base12_S_atoms_outE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
+#endif
 #if defined __powerpc__ && !defined __powerpc64__
 	{"_ZNSt10__num_base12_S_atoms_outE","GLIBCXX_3.4",0,LSB_Cpp,4,0},
 #endif
@@ -5468,13 +5471,10 @@ struct versym libstdcxx_so_6[] = {
 #if defined __s390__ && !defined __s390x__
 	{"_ZNSt10__num_base12_S_atoms_outE","GLIBCXX_3.4",0,LSB_Cpp,4,0},
 #endif
-#if defined __s390x__
-	{"_ZNSt10__num_base12_S_atoms_outE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
-#endif
-#if defined __ia64__
-	{"_ZNSt10__num_base12_S_atoms_outE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
-#endif
 #if defined __x86_64__
+	{"_ZNSt10__num_base12_S_atoms_outE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
+#endif
+#if defined __s390x__
 	{"_ZNSt10__num_base12_S_atoms_outE","GLIBCXX_3.4",0,LSB_Cpp,8,0},
 #endif
 	{"_ZNSt10__num_base15_S_format_floatERKSt8ios_basePcc","GLIBCXX_3.4",0,LSB_Cpp,0,1},
@@ -8728,19 +8728,19 @@ struct versym libstdcxx_so_6[] = {
 #if defined __ia64__
 	{"_ZNSt17__timepunct_cacheIwE12_S_timezonesE","GLIBCXX_3.4",0,LSB_Cpp,112,0},
 #endif
-#if defined __powerpc64__
-	{"_ZNSt17__timepunct_cacheIwE12_S_timezonesE","GLIBCXX_3.4",0,LSB_Cpp,112,0},
-#endif
 #if defined __powerpc__ && !defined __powerpc64__
 	{"_ZNSt17__timepunct_cacheIwE12_S_timezonesE","GLIBCXX_3.4",0,LSB_Cpp,56,0},
+#endif
+#if defined __powerpc64__
+	{"_ZNSt17__timepunct_cacheIwE12_S_timezonesE","GLIBCXX_3.4",0,LSB_Cpp,112,0},
 #endif
 #if defined __s390__ && !defined __s390x__
 	{"_ZNSt17__timepunct_cacheIwE12_S_timezonesE","GLIBCXX_3.4",0,LSB_Cpp,56,0},
 #endif
-#if defined __s390x__
+#if defined __x86_64__
 	{"_ZNSt17__timepunct_cacheIwE12_S_timezonesE","GLIBCXX_3.4",0,LSB_Cpp,112,0},
 #endif
-#if defined __x86_64__
+#if defined __s390x__
 	{"_ZNSt17__timepunct_cacheIwE12_S_timezonesE","GLIBCXX_3.4",0,LSB_Cpp,112,0},
 #endif
 	{"_ZNSt17moneypunct_bynameIcLb0EE4intlE","GLIBCXX_3.4",0,LSB_Cpp,0,0},
