4331616c0bc85f31a96109ed63bd80e6028a0e2a
Mats Wichmann <mats@freestandards.org>
2009-05-06 06:50:08 -0600

    more devchk inc files to support XKB additions (bug 2525)

--- devchk/ts/devchk/X11_extensions_securstr_h.c
+++ devchk/ts/devchk/X11_extensions_securstr_h.c
@@ -8,6 +8,10 @@
 #include <sys/types.h>
 #define _LSB_DEFAULT_ARCH 1
 #define __LSB_VERSION__ 40
+#include <X11/X.h>
+#include <X11/Xauth.h>
+#include <X11/Xlib.h>
+#include <X11/Xmd.h>
 #include "X11/extensions/securstr.h"
 
 
