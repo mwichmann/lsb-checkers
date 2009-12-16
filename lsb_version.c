77df4d32dbb9c83a58a7c54ee71516c139147818
Mats Wichmann <mats@freestandards.org>
2009-12-16 09:00:11 -0700

    finally enable LSB=4.1

--- lsb_version.c
+++ lsb_version.c
@@ -1,8 +1,8 @@
-char* LSB_Versions[] = {"3.0", "3.1", "3.2", "4.0"};
-char* LSB_Versions_list = "3.0, 3.1, 3.2, 4.0";
+char* LSB_Versions[] = {"3.0", "3.1", "3.2", "4.0", "4.1"};
+char* LSB_Versions_list = "3.0, 3.1, 3.2, 4.0, 4.1";
 int num_LSB_Versions = sizeof(LSB_Versions)/sizeof(char *);
 
 int LSB_Version = -1;
 
-/* Default LSB version is 4.0 */
-int LSB_Version_default = 3;
+/* Default LSB version is 4.1 */
+int LSB_Version_default = 4;
