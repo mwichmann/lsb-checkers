cd3319d61bd79aee43f983d43d5e1eb2c397ab60
Mats Wichmann <mats@freestandards.org>
2010-08-11 12:04:09 -0600

    make libchk generated lib srcs cleanable

--- libchk/libs.mk
+++ libchk/libs.mk
@@ -66,4 +66,73 @@ LIBOBJS = \
 	libXtst_so_6.o \
 	libz_so_1.o \
 
+
+LIBSRCS = \
+	ld_lsb_ia64_so_3.c \
+	ld_lsb_ppc32_so_3.c \
+	ld_lsb_s390_so_3.c \
+	ld_lsb_so_3.c \
+	ld_lsb_ppc64_so_3.c \
+	ld_lsb_s390x_so_3.c \
+	ld_lsb_x86_64_so_3.c \
+	libasound_so_2.c \
+	libatk_1_0_so_0.c \
+	libc_so_6.c \
+	libc_so_6_1.c \
+	libcairo_so_2.c \
+	libcrypt_so_1.c \
+	libcups_so_2.c \
+	libcupsimage_so_2.c \
+	libdl_so_2.c \
+	libfontconfig_so_1.c \
+	libfreetype_so_6.c \
+	libgcc_s_so_1.c \
+	libgdk_x11_2_0_so_0.c \
+	libgdk_pixbuf_2_0_so_0.c \
+	libgdk_pixbuf_xlib_2_0_so_0.c \
+	libGL_so_1.c \
+	libglib_2_0_so_0.c \
+	libGLU_so_1.c \
+	libgmodule_2_0_so_0.c \
+	libgobject_2_0_so_0.c \
+	libgthread_2_0_so_0.c \
+	libgtk_x11_2_0_so_0.c \
+	libICE_so_6.c \
+	libjpeg_so_62.c \
+	libm_so_6.c \
+	libm_so_6_1.c \
+	libncurses_so_5.c \
+	libnspr4_so.c \
+	libnss3_so.c \
+	libpam_so_0.c \
+	libpango_1_0_so_0.c \
+	libpangocairo_1_0_so_0.c \
+	libpangoft2_1_0_so_0.c \
+	libpangoxft_1_0_so_0.c \
+	libpng12_so_0.c \
+	libpthread_so_0.c \
+	libqt_mt_so_3.c \
+	libQtCore_so_4.c \
+	libQtGui_so_4.c \
+	libQtNetwork_so_4.c \
+	libQtOpenGL_so_4.c \
+	libQtSql_so_4.c \
+	libQtSvg_so_4.c \
+	libQtXml_so_4.c \
+	librt_so_1.c \
+	libSM_so_6.c \
+	libssl3_so.c \
+	libstdcxx_so_6.c \
+	libutil_so_1.c \
+	libX11_so_6.c \
+	libXext_so_6.c \
+	libXft_so_2.c \
+	libXi_so_6.c \
+	libxml2_so_2.c \
+	libXrender_so_1.c \
+	libXt_so_6.c \
+	libXtst_so_6.c \
+	libz_so_1.c \
+
+
 LSBLIBRARIES= -lasound -latk-1.0 -lc -lcairo -lcrypt -lcups -lcupsimage -ldl -lfontconfig -lfreetype -lgcc_s -lgdk-x11-2.0 -lgdk_pixbuf-2.0 -lgdk_pixbuf_xlib-2.0 -lGL -lglib-2.0 -lGLU -lgmodule-2.0 -lgobject-2.0 -lgthread-2.0 -lgtk-x11-2.0 -lICE -ljpeg -lm -lncurses -lnspr4 -lnss3 -lpam -lpango-1.0 -lpangocairo-1.0 -lpangoft2-1.0 -lpangoxft-1.0 -lpng12 -lpthread -lqt-mt -lQtCore -lQtGui -lQtNetwork -lQtOpenGL -lQtSql -lQtSvg -lQtXml -lrt -lSM -lssl3 -lstdc++ -lutil -lX11 -lXext -lXft -lXi -lxml2 -lXrender -lXt -lXtst -lz
