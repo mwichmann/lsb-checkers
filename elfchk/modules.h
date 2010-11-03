c2a0dbcebab88fe4ecde47d501a66226ac99d195
Mats Wichmann <mats@freestandards.org>
2010-11-03 14:36:59 -0600

    move Security smod in (bug 3072); fix handling of Java smod (bug 3076)

--- elfchk/modules.h
+++ elfchk/modules.h
@@ -24,11 +24,12 @@ extern struct lsb_module* LSB_Modules[];
 #define LSB_Perl	0x800
 #define LSB_Multimedia	0x1000
 #define LSB_Security	0x2000
+#define LSB_Java	0x4000
 #define LSB_Core_Modules 	(LSB_Core | LSB_Cpp)
 #define LSB_Desktop_Modules_LSB30 (LSB_Core | LSB_Graphics | LSB_Cpp)
 #define LSB_Desktop_Modules_LSB31 (LSB_Core | LSB_Graphics | LSB_Cpp | LSB_Toolkit_Gtk | LSB_Graphics_Ext | LSB_XML | LSB_Toolkit_Qt3)
 #define LSB_Desktop_Modules_LSB32 (LSB_Core | LSB_Graphics | LSB_Cpp | LSB_Toolkit_Gtk | LSB_Toolkit_Qt | LSB_Graphics_Ext | LSB_XML | LSB_Toolkit_Qt3 | LSB_Printing | LSB_Python | LSB_Perl)
-#define LSB_Desktop_Modules_LSB40 (LSB_Core | LSB_Graphics | LSB_Cpp | LSB_Toolkit_Gtk | LSB_Toolkit_Qt | LSB_Graphics_Ext | LSB_XML | LSB_Toolkit_Qt3 | LSB_Printing | LSB_Python | LSB_Perl | LSB_Security)
+#define LSB_Desktop_Modules_LSB40 (LSB_Core | LSB_Graphics | LSB_Cpp | LSB_Toolkit_Gtk | LSB_Toolkit_Qt | LSB_Graphics_Ext | LSB_XML | LSB_Toolkit_Qt3 | LSB_Printing | LSB_Python | LSB_Perl)
 #define LSB_Desktop_Modules_LSB41 (LSB_Core | LSB_Graphics | LSB_Cpp | LSB_Toolkit_Gtk | LSB_Toolkit_Qt | LSB_Graphics_Ext | LSB_XML | LSB_Toolkit_Qt3 | LSB_Printing | LSB_Python | LSB_Perl | LSB_Multimedia | LSB_Security)
 #define LSB_All_Modules 0xffff
 
