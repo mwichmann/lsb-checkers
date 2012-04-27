7d03c29f71ded627a58cd5df75182c57f69e2294
Denis Silakov <denis.silakov@rosalab.ru>
2012-04-27 16:23:39 +0400

    One more fix for bug #3500

--- elfchk/dwarf.c
+++ elfchk/dwarf.c
@@ -160,7 +160,7 @@ check_CFI(unsigned char *ptr, int *error, int ptr_encoding)
 
 	/* Operand 1 - address */
 	if (ptr_encoding) {
-/*	    tmpaddr = (long) read_FDE_encoded(ptr, ptr_encoding, &numused);*/
+/*	    tmpaddr = (long) */read_FDE_encoded(ptr, ptr_encoding, &numused);
 	    ptr += numused;
 	    used += numused;
 	} else {
