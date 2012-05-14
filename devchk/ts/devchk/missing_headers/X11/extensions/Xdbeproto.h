6336f708ff00634e84aed7f84c367c4a9a2ff0cb
Mats Wichmann <mats@linuxfoundation.org>
2012-05-14 12:41:13 -0600

    devchk has split to a separate branch (bug 3160)

--- devchk/ts/devchk/missing_headers/X11/extensions/Xdbeproto.h
+++
@@ -1,4 +0,0 @@
-#include <X11/Xlib.h>
-#include <X11/extensions/dbe.h>
-#include <X11/extensions/dbeproto.h>
-#include <X11/extensions/Xdbe.h>
--- devchk/ts/devchk/missing_headers/X11/extensions/securstr.h
+++
@@ -1,3 +0,0 @@
-#include <X11/extensions/secur.h>
-#include <X11/extensions/securproto.h>
-#define _SECURSTR_H -> _SECUR_H
