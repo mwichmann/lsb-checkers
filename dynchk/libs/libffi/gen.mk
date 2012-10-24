b49445a943413d68b3468de67d21fadf05452bf6
Mats Wichmann <mats@linuxfoundation.org>
2012-10-24 11:25:41 -0600

    forgot to 'add' libffi and new glib files in dynchk

---
+++ dynchk/libs/libffi/gen.mk
@@ -0,0 +1,20 @@
+GEN_FUNCS = \
+ffi_call.o \
+ffi_closure_alloc.o \
+ffi_closure_free.o \
+ffi_java_ptrarray_to_raw.o \
+ffi_java_raw_call.o \
+ffi_java_raw_size.o \
+ffi_java_raw_to_ptrarray.o \
+ffi_prep_cif.o \
+ffi_prep_cif_var.o \
+ffi_prep_closure.o \
+ffi_prep_closure_loc.o \
+ffi_prep_java_raw_closure.o \
+ffi_prep_java_raw_closure_loc.o \
+ffi_prep_raw_closure.o \
+ffi_prep_raw_closure_loc.o \
+ffi_ptrarray_to_raw.o \
+ffi_raw_call.o \
+ffi_raw_size.o \
+ffi_raw_to_ptrarray.o 
